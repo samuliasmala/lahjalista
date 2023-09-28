5ea9b1f3e57b694f6b4635582e9b0291588a39bb
linux <linux@pop-os.localdomain>
2023-09-28 10:02:22 +0300

    Deleted a comment inside Label component

--- components/Label.tsx
+++ components/Label.tsx
@@ -1,20 +1,5 @@
 import { ReactNode } from 'react';
 
-/*
-<div id="receiverListContainer" className="mt-3">
-          <div id="receiverTitle" className="text-2xl pt-4">
-            Lahjaideat
-          </div>
-          <div id="giftData">
-            {giftData.map(giftData => (
-              <li key={giftData.id}>
-                {giftData.name} - {giftData.gift}
-              </li>
-            ))}
-          </div>
-        </div>
-
-*/
 
 export function Label({
   id,
