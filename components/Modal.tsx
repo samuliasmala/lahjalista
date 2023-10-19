a9952694aa2387d1e4dd859861680e5daea8db0d
Antti Asmala <linux@pop-os.localdomain>
2023-10-19 13:44:05 +0300

    Adding code to Modal.tsx. Added Button component, an useState and a check for type

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,5 +1,6 @@
-import { ButtonHTMLAttributes } from "react";
+import { ButtonHTMLAttributes, useState } from "react";
 import { FullLocalStorage } from "~/pages";
+import { Button } from "./Button";
 
 
 
@@ -14,5 +15,28 @@ export function Modal({
     ...rest
 }: ModalType
 ) {
-    
+    const [openWindow, setOpenWindow] = useState(false);
+    if(typeof gift === "undefined") return null
+    return(
+        <>
+        <Button
+        key={`${gift.id}_deletebutton`}
+        onMouseOver={(e) => {
+          // can use statement *as* here due to the button being inside of the li parentElement
+          (e.currentTarget.parentElement as HTMLElement).className =
+            'line-through';
+        }}
+        onMouseOut={(e) => {
+          // can use statement *as* here due to the button being inside of the li parentElement
+          (e.currentTarget.parentElement as HTMLElement).className = '';
+        }}
+        className="ms-5 p-0 w-16 h-8 hover:text-red-600"
+        onClick={() => setOpenWindow(true)}
+        type="button"
+        {...rest}
+      >
+        {children}
+      </Button>
+        </>
+    )
 }
