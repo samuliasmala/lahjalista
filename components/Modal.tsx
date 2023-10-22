1cd5af0741926d875ee7d6377e93f1b41707756d
Antti Asmala <linux@pop-os.localdomain>
2023-10-22 20:27:15 +0300

    Refactoring Modal.tsx. Changed SVG HTML element to decline button component

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -9,6 +9,7 @@ import {
 } from '~/utils/localStorageFunctions';
 import Image from 'next/image';
 import AcceptButtonSVG from '../public/images/icons/accept_button.svg';
+import DeclineButtonSVG from '../public/images/icons/decline_button.svg';
 
 type ModalType = ButtonHTMLAttributes<HTMLButtonElement> & {
   gift: FullLocalStorage;
@@ -97,31 +98,33 @@ export function Modal({
               className="relative mt-3 row-start-3 row-end-3 col-start-1 col-end-1 left-5 sm:left-5"
               onClick={() => handleDeletion()}
               onMouseOver={(e: React.MouseEvent<SVGElement, MouseEvent>) =>
-                e.currentTarget.classList.add("[&_:nth-child(1)]:fill-yellow-400")
+                e.currentTarget.classList.add(
+                  '[&_:nth-child(1)]:fill-yellow-400',
+                )
               }
               onMouseOut={(e: React.MouseEvent<SVGElement, MouseEvent>) =>
-                e.currentTarget.classList.remove("[&_:nth-child(1)]:fill-yellow-400")
+                e.currentTarget.classList.remove(
+                  '[&_:nth-child(1)]:fill-yellow-400',
+                )
               }
             />
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="64"
-              height="64"
-              viewBox="0 0 24 24"
+            <DeclineButtonSVG
+              alt="declineButton"
+              width={64}
+              height={64}
               className="relative mt-3 row-start-3 row-end-3 col-start-1 col-end-1 left-32 sm:left-28"
-            >
-              <path
-                onClick={() => setOpenWindow(false)}
-                onMouseOver={(e) =>
-                  e.currentTarget.setAttribute('fill', '#60946e')
-                }
-                onMouseOut={(e) =>
-                  e.currentTarget.setAttribute('fill', 'currentColor')
-                }
-                fill="currentColor"
-                d="m8.4 17l3.6-3.6l3.6 3.6l1.4-1.4l-3.6-3.6L17 8.4L15.6 7L12 10.6L8.4 7L7 8.4l3.6 3.6L7 15.6L8.4 17Zm3.6 5q-2.075 0-3.9-.788t-3.175-2.137q-1.35-1.35-2.137-3.175T2 12q0-2.075.788-3.9t2.137-3.175q1.35-1.35 3.175-2.137T12 2q2.075 0 3.9.788t3.175 2.137q1.35 1.35 2.138 3.175T22 12q0 2.075-.788 3.9t-2.137 3.175q-1.35 1.35-3.175 2.138T12 22Z"
-              />
-            </svg>
+              onClick={() => setOpenWindow(false)}
+              onMouseOver={(e: React.MouseEvent<SVGElement, MouseEvent>) =>
+                e.currentTarget.classList.add(
+                  '[&_:nth-child(1)]:fill-yellow-400',
+                )
+              }
+              onMouseOut={(e: React.MouseEvent<SVGElement, MouseEvent>) =>
+                e.currentTarget.classList.remove(
+                  '[&_:nth-child(1)]:fill-yellow-400',
+                )
+              }
+            />
           </Container>
         </Container>
       ) : null}
