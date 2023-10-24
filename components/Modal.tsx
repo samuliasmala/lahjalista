ebbb8a68e22c547ec8543e39970f5f93f92d8517
Antti Asmala <linux@pop-os.localdomain>
2023-10-24 16:36:07 +0300

    Refactoring Modal.tsx. Deleted unnecessary lines of code

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,23 +1,12 @@
 import { HTMLAttributes, useState } from 'react';
 import { Container } from './Container';
 
-type ModalType = HTMLAttributes<HTMLDivElement> & {
-  openWindow?: boolean;
-}
-
-
-export function Modal({ openWindow, children, ...rest }: ModalType) {
-  //const [openWindow, setOpenWindow] = useState(false);
-
+export function Modal({ children, ...rest }: HTMLAttributes<HTMLDivElement>) {
   return (
-    <>
-      {openWindow ? (
-        <Container className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] ">
-          <Container className="grid w-96 sm:w-96 border border-yellow-300 bg-gray-200">
-            {children}
-          </Container>
-        </Container>
-      ) : null}
-    </>
+    <Container className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] ">
+      <Container className="grid w-96 sm:w-96 border border-yellow-300 bg-gray-200">
+        {children}
+      </Container>
+    </Container>
   );
 }
