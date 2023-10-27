147f1234120837e3bed80a24e59eb586ae0cc054
Antti Asmala <linux@pop-os.localdomain>
2023-10-27 10:45:56 +0300

    Refactoring DeleteModal.tsx. Added a button element on SVG images. Changed styling of the buttons a bit

--- components/DeleteModal.tsx
+++ components/DeleteModal.tsx
@@ -1,6 +1,4 @@
 import { TitleText } from './TitleText';
-import AcceptButtonSVG from '../public/images/icons/accept_button.svg';
-import DeclineButtonSVG from '../public/images/icons/decline_button.svg';
 import React, { ButtonHTMLAttributes, Dispatch, SetStateAction } from 'react';
 import { FullLocalStorage } from '~/pages';
 import {
@@ -8,6 +6,8 @@ import {
   setLocalStorage,
 } from '~/utils/localStorageFunctions';
 import { Modal } from './Modal';
+import SvgAcceptButtonIcon from '~/icons/accept_button_icon';
+import SvgDeclineButtonIcon from '~/icons/decline_button_icon';
 
 type ModalType = ButtonHTMLAttributes<HTMLButtonElement> & {
   gift?: FullLocalStorage;
@@ -47,32 +47,56 @@ export function DeleteModal({
       <p className="row-start-2 row-end-2 ps-5 pt-5 text-lg w-full h-full font-bold">
         {dataToDeleteInfo}
       </p>
-      <AcceptButtonSVG
-        alt="acceptButton"
-        width={64}
-        height={64}
-        className="relative mt-3 row-start-3 row-end-3 col-start-1 col-end-1 left-5 sm:left-5"
-        onClick={() => handleDeletion()}
-        onMouseOver={(e: React.MouseEvent<SVGElement, MouseEvent>) =>
-          e.currentTarget.classList.add('[&_:nth-child(1)]:fill-yellow-400')
-        }
-        onMouseOut={(e: React.MouseEvent<SVGElement, MouseEvent>) =>
-          e.currentTarget.classList.remove('[&_:nth-child(1)]:fill-yellow-400')
-        }
-      />
-      <DeclineButtonSVG
-        alt="declineButton"
-        width={64}
-        height={64}
-        className="relative mt-3 row-start-3 row-end-3 col-start-1 col-end-1 left-32 sm:left-28"
-        onClick={() => closeModalUseState(false)}
-        onMouseOver={(e: React.MouseEvent<SVGElement, MouseEvent>) =>
-          e.currentTarget.classList.add('[&_:nth-child(1)]:fill-yellow-400')
-        }
-        onMouseOut={(e: React.MouseEvent<SVGElement, MouseEvent>) =>
-          e.currentTarget.classList.remove('[&_:nth-child(1)]:fill-yellow-400')
-        }
-      />
+      <button
+        className="border border-yellow-500 mt-3 row-start-3 row-end-3 col-start-1 col-end-1 w-[64px] h-[64px] bg-gray-300"
+        onMouseOver={(e) => {
+          e.currentTarget.classList.remove('bg-gray-300');
+          e.currentTarget.classList.add('bg-gray-600');
+        }}
+        onMouseOut={(e) => {
+          e.currentTarget.classList.remove('bg-gray-600');
+          e.currentTarget.classList.add('bg-gray-300');
+        }}
+      >
+        <SvgAcceptButtonIcon
+          width={64}
+          height={64}
+          onClick={() => handleDeletion()}
+          onMouseOver={(e: React.MouseEvent<SVGElement, MouseEvent>) =>
+            e.currentTarget.classList.add('[&_:nth-child(1)]:fill-yellow-400')
+          }
+          onMouseOut={(e: React.MouseEvent<SVGElement, MouseEvent>) =>
+            e.currentTarget.classList.remove(
+              '[&_:nth-child(1)]:fill-yellow-400',
+            )
+          }
+        />
+      </button>
+      <button
+        className="border border-yellow-500 relative mt-3 left-32 sm:left-28 row-start-3 row-end-3 col-start-1 col-end-1 w-[64px] h-[64px] bg-gray-300"
+        onMouseOver={(e) => {
+          e.currentTarget.classList.remove('bg-gray-300');
+          e.currentTarget.classList.add('bg-gray-600');
+        }}
+        onMouseOut={(e) => {
+          e.currentTarget.classList.remove('bg-gray-600');
+          e.currentTarget.classList.add('bg-gray-300');
+        }}
+      >
+        <SvgDeclineButtonIcon
+          width={64}
+          height={64}
+          onClick={() => closeModalUseState(false)}
+          onMouseOver={(e: React.MouseEvent<SVGElement, MouseEvent>) =>
+            e.currentTarget.classList.add('[&_:nth-child(1)]:fill-yellow-400')
+          }
+          onMouseOut={(e: React.MouseEvent<SVGElement, MouseEvent>) =>
+            e.currentTarget.classList.remove(
+              '[&_:nth-child(1)]:fill-yellow-400',
+            )
+          }
+        />
+      </button>
     </Modal>
   );
 }
