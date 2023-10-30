244c244ab8ec99a82ebdd724802abaca03b60e55
Antti Asmala <linux@pop-os.localdomain>
2023-10-30 09:09:31 +0200

    Refactoring accept and decline button .tsx-files. Added props for changing fill colors

--- icons/accept_button_icon.tsx
+++ icons/accept_button_icon.tsx
@@ -1,12 +1,22 @@
 import type { SVGProps } from 'react';
-const SvgAcceptButtonIcon = (props: SVGProps<SVGSVGElement>) => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" {...props}>
+
+type AcceptButtonType = SVGProps<SVGSVGElement> & {
+  backgroundFill?: string;
+  checkmarkFill?: string;
+};
+
+const SvgAcceptButtonIcon = ({
+  backgroundFill,
+  checkmarkFill,
+  ...rest
+}: AcceptButtonType) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" {...rest}>
     <path
-      fill="currentColor"
+      fill={backgroundFill || 'currentColor'}
       d="M16 2a14 14 0 1 0 14 14A14 14 0 0 0 16 2Zm-2 19.59-5-5L10.59 15 14 18.41 21.41 11l1.596 1.586Z"
     />
     <path
-      fill="none"
+      fill={checkmarkFill || 'none'}
       d="m14 21.591-5-5L10.591 15 14 18.409 21.41 11l1.595 1.585L14 21.591z"
     />
   </svg>
