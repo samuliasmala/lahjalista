ed20f50d6b711788a5d32e50b82b85642ccaee93
Antti Asmala <linux@pop-os.localdomain>
2023-11-09 14:03:00 +0200

    Refactoring jsonServerFunctions.ts. Changed the way how getOne function works. Added JSDoc information

--- utils/jsonServerFunctions.ts
+++ utils/jsonServerFunctions.ts
@@ -6,16 +6,29 @@ async function getAll() {
     return await axios.get(baseURL)
 }
 
-async function getOne(id: string) {
-    return (await axios.get(`${baseURL}/${id}`)).data
+/**
+ * 
+ * @param searchParamteres should be given a string that contains search paramteres as query strings. 
+ * @example name=Foo&gift=Bar
+ * @returns an array of JavaScript objects if found. Else an empty array
+ */
+async function getOne(searchParamteres: string) {
+    return (await axios.get(`${baseURL}?${searchParamteres}`)).data
 }
 
 async function create(newObject: object) {
     return (await axios.post(baseURL, newObject)).data
 }
 
-async function update(id: string, newObject: object) {
-    return (await axios.put(`${baseURL}/${id}`, newObject)).data
+/**
+ * 
+ * @param searchParamteres should be given a string that contains search paramteres as query strings. 
+ * @example name=Foo&gift=Bar
+ * @param newObject a new object that will be replacing the old object
+ * @returns an array of JavaScript objects after the update
+ */
+async function update(searchParamteres: string, newObject: object) {
+    return (await axios.put(`${baseURL}?${searchParamteres}`, newObject)).data
 }
 
 
