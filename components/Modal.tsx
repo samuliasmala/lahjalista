7aedb4dd88dc6b8c9a4cd38eba006af9e75b81db
Antti Asmala <linux@pop-os.localdomain>
2023-11-29 10:36:18 +0200

    Added functionality to get the focus to Modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,12 +1,17 @@
 import { HTMLAttributes } from 'react';
-import { Container } from './Container';
+import { createPortal } from 'react-dom';
 
 export function Modal({ children }: HTMLAttributes<HTMLDivElement>) {
-  return (
-    <Container className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] ">
-      <Container className="grid w-96 sm:w-96 border border-yellow-300 bg-gray-200">
-        {children}
-      </Container>
-    </Container>
+  return createPortal(
+    <>
+      <div className="fixed left-0 top-0 z-[98] h-full w-full bg-black opacity-20">
+        <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] ">
+          <div className="grid w-96 sm:w-96 border border-yellow-300 bg-gray-200">
+            {children}
+          </div>
+        </div>
+      </div>
+    </>,
+    document.body,
   );
 }
