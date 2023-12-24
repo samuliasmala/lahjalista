528d7eecd4974ae82e2f3a5c608579070febfa70
Antti Asmala <linux@pop-os.localdomain>
2023-12-25 00:28:16 +0200

    ESLint fixes

--- icons/accept_button_icon.tsx
+++ icons/accept_button_icon.tsx
@@ -1,4 +1,5 @@
 import type { SVGProps } from 'react';
+
 const SvgAcceptButtonIcon = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" {...props}>
     <path
