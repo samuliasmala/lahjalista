54c6bf066e82bae2243db4a369cc2b4de3735364
Antti Asmala <linux@pop-os.localdomain>
2024-01-08 15:55:00 +0200

    Removed export default when exporting functions in jsonServerFunctions.ts. Exported every function by using export individually

--- utils/jsonServerFunctions.ts
+++ utils/jsonServerFunctions.ts
@@ -3,7 +3,7 @@ import { Gifts } from '~/pages';
 
 const baseURL = 'http://localhost:3001/gifts';
 
-async function getAll() {
+export async function getAll() {
   return ((await axios.get(baseURL)).data) as Gifts[];
 }
 
@@ -12,11 +12,11 @@ async function getAll() {
  * @param id should be given a string that contains the id that is wanted to be searched.
  * @returns an array of JavaScript objects if found. Else an empty array
  */
-async function getOne(id: string) {
+export async function getOne(id: string) {
   return ((await axios.get(`${baseURL}?id=${id}`)).data) as Gifts[];
 }
 
-async function create(newObject: object) {
+export async function create(newObject: object) {
   return (await axios.post(baseURL, newObject)).data;
 }
 
@@ -26,7 +26,7 @@ async function create(newObject: object) {
  * @param newObject a new object of Gifts that will be replacing the old object
  * @returns an array of JavaScript objects after the update
  */
-async function update(id: string, newObject: Gifts[]) {
+export async function update(id: string, newObject: Gifts[]) {
   return (await axios.put(`${baseURL}?${id}`, newObject)).data;
 }
 
@@ -35,10 +35,6 @@ async function update(id: string, newObject: Gifts[]) {
  * @param id should be given the id that is wanted to be removed
  * @returns
  */
-async function remove(id: string) {
+export async function remove(id: string) {
   return (await axios.delete(`${baseURL}/${id}`)).data;
 }
-
-const exportableModules = { getAll, getOne, create, update, remove };
-
-export default exportableModules;
