9dc8f03204a6d890ce370f9045583f18fdb59882
Antti Asmala <antti.asmala@gmail.com>
2024-01-25 12:16:46 +0200

    Created a new file moveInputCursorToEnd.ts. Created a function that moves the input's cursor to the end

---
+++ utils/moveInputCursorToEnd.ts
@@ -0,0 +1,8 @@
+
+/**
+ * 
+ * @param e give the event value of onClick or onFocus props
+ */
+export function moveInputCursorToEnd(e: React.MouseEvent<HTMLInputElement, MouseEvent> | React.FocusEvent<HTMLInputElement, Element>) {
+    e.currentTarget.setSelectionRange(e.currentTarget.value.length, e.currentTarget.value.length)
+}
