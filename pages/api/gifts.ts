cbb31e156370a23bace3b8499dcdcfd928139ff3
Antti Asmala <antti.asmala@gmail.com>
2024-01-26 23:10:26 +0200

    Changed PUT and DELETE spots in case-switch. Created a placeholder functions for every request type and some basic test code

--- pages/api/gifts.ts
+++ pages/api/gifts.ts
@@ -1,27 +1,50 @@
+import axios from "axios";
 import { NextApiRequest, NextApiResponse } from "next";
+import { Gift } from "..";
 
+const baseURL = "http://localhost:3001/gifts"
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     switch (req.method) {
         case "GET":
-            handleGET(req.query)
+            await handleGET(req, res)
             break
         case "POST":
-            console.log(req.method, "POST2")
-            break
-        case "DELETE":
-            console.log("DELETE")
+            await handlePOST(req, res)
             break
         case "PUT":
-            console.log("PUT")
+            await handlePUT(req, res)
+            break
+        case "DELETE":
+            await handleDELETE(req, res)
             break
         default:
             console.log(req.method, "default")
             break
     }
-    res.status(200).json({ message: "Hello from Next.js!" })
 }
 
-function handleGET(queries: NextApiRequest["query"]) {
-    console.log(queries)
+async function handleGET(req: NextApiRequest, res: NextApiResponse) {
+    if (req.query.id !== undefined) {
+        const giftRequest = await axios.get(`http://localhost:3001/gifts/${req.query.id}`)
+        const giftItem = giftRequest.data as Gift
+        return res.status(giftRequest.status).json(giftItem)
+    }
+    const gifts = await axios.get(baseURL)
+    return res.status(gifts.status).json(gifts.data)
+}
+
+async function handlePOST(req: NextApiRequest, res: NextApiResponse) {
+    const postRequest = await axios.post(baseURL, req.body)
+    return res.status(postRequest.status).json(req.body)
+}
+
+async function handlePUT(req: NextApiRequest, res: NextApiResponse) {
+    const putRequest = await axios.put(`${baseURL}/${req.body.id}`, req.body)
+    return res.status(putRequest.status).json(req.body)
+}
+
+async function handleDELETE(req: NextApiRequest, res: NextApiResponse) {
+    const deleteRequest = await axios.delete(`${baseURL}/${req.body.id}`)
+    return res.status(deleteRequest.status).json(req.body)
 }
