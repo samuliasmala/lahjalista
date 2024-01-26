6eca4ff7b6a4f7a3d824e40c229aec2420b5b585
Antti Asmala <antti.asmala@gmail.com>
2024-01-26 09:15:20 +0200

    Created a switch for every request method. Created as well a handler for GET request and tested getting queries

--- pages/api/gifts.ts
+++ pages/api/gifts.ts
@@ -2,5 +2,26 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    switch (req.method) {
+        case "GET":
+            handleGET(req.query)
+            break
+        case "POST":
+            console.log(req.method, "POST2")
+            break
+        case "DELETE":
+            console.log("DELETE")
+            break
+        case "PUT":
+            console.log("PUT")
+            break
+        default:
+            console.log(req.method, "default")
+            break
+    }
     res.status(200).json({ message: "Hello from Next.js!" })
+}
+
+function handleGET(queries: NextApiRequest["query"]) {
+    console.log(queries)
 }
