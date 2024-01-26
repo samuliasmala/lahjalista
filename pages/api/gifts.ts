74e9d8bbc0af4654488b6fb156e8eac7d507da9f
Antti Asmala <antti.asmala@gmail.com>
2024-01-26 23:33:09 +0200

    Added a new variable queryId and gave it a type of string due to ESLint errors

--- pages/api/gifts.ts
+++ pages/api/gifts.ts
@@ -1,50 +1,58 @@
-import axios from "axios";
-import { NextApiRequest, NextApiResponse } from "next";
-import { Gift } from "..";
+import axios from 'axios';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { Gift } from '..';
 
-const baseURL = "http://localhost:3001/gifts"
+const baseURL = 'http://localhost:3001/gifts';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    switch (req.method) {
-        case "GET":
-            await handleGET(req, res)
-            break
-        case "POST":
-            await handlePOST(req, res)
-            break
-        case "PUT":
-            await handlePUT(req, res)
-            break
-        case "DELETE":
-            await handleDELETE(req, res)
-            break
-        default:
-            console.log(req.method, "default")
-            break
-    }
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  switch (req.method) {
+    case 'GET':
+      await handleGET(req, res);
+      break;
+    case 'POST':
+      await handlePOST(req, res);
+      break;
+    case 'PUT':
+      await handlePUT(req, res);
+      break;
+    case 'DELETE':
+      await handleDELETE(req, res);
+      break;
+    default:
+      console.log(req.method, 'default');
+      break;
+  }
 }
 
 async function handleGET(req: NextApiRequest, res: NextApiResponse) {
-    if (req.query.id !== undefined) {
-        const giftRequest = await axios.get(`http://localhost:3001/gifts/${req.query.id}`)
-        const giftItem = giftRequest.data as Gift
-        return res.status(giftRequest.status).json(giftItem)
-    }
-    const gifts = await axios.get(baseURL)
-    return res.status(gifts.status).json(gifts.data)
+  if (req.query.id !== undefined) {
+    const queryId = req.query.id as string;
+    const giftRequest = await axios.get(
+      `http://localhost:3001/gifts/${queryId}`,
+    );
+    const giftItem = giftRequest.data as Gift;
+    return res.status(giftRequest.status).json(giftItem);
+  }
+  const gifts = await axios.get(baseURL);
+  return res.status(gifts.status).json(gifts.data);
 }
 
 async function handlePOST(req: NextApiRequest, res: NextApiResponse) {
-    const postRequest = await axios.post(baseURL, req.body)
-    return res.status(postRequest.status).json(req.body)
+  const postRequest = await axios.post(baseURL, req.body);
+  return res.status(postRequest.status).json(req.body);
 }
 
 async function handlePUT(req: NextApiRequest, res: NextApiResponse) {
-    const putRequest = await axios.put(`${baseURL}/${req.body.id}`, req.body)
-    return res.status(putRequest.status).json(req.body)
+  const queryId = req.query.id as string;
+  const putRequest = await axios.put(`${baseURL}/${queryId}`, req.body);
+  return res.status(putRequest.status).json(req.body);
 }
 
 async function handleDELETE(req: NextApiRequest, res: NextApiResponse) {
-    const deleteRequest = await axios.delete(`${baseURL}/${req.body.id}`)
-    return res.status(deleteRequest.status).json(req.body)
+  const queryId = req.query.id as string;
+  const deleteRequest = await axios.delete(`${baseURL}/${queryId}`);
+  return res.status(deleteRequest.status).json(req.body);
 }
