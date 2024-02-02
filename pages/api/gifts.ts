ca33ad22b34d0dda375908f15be8d3ea355ad7fd
Antti Asmala <antti.asmala@gmail.com>
2024-02-02 12:25:01 +0200

    Added try/catch blocks to rest of the handlers

--- pages/api/gifts.ts
+++ pages/api/gifts.ts
@@ -47,12 +47,15 @@ export default async function handler(
 
 async function handleGET(req: NextApiRequest, res: NextApiResponse) {
   if (req.query.id !== undefined) {
-    const queryId = req.query.id as string;
-    const giftRequest = await axios.get(
-      `http://localhost:3001/gifts/${queryId}`,
-    );
-    const giftItem = giftRequest.data as Gift;
-    return res.status(giftRequest.status).json(giftItem);
+    try {
+      const queryId = req.query.id as string;
+      const giftRequest = await axios.get(
+        `http://localhost:3001/gifts/${queryId}`,
+      );
+      return res.status(giftRequest.status).json(giftRequest.data as Gift);
+    } catch (e) {
+      return catchError(res, e);
+    }
   }
   const gifts = await axios.get(baseURL);
   return res.status(gifts.status).json(gifts.data);
@@ -64,15 +67,23 @@ async function handlePOST(req: NextApiRequest, res: NextApiResponse) {
 }
 
 async function handlePATCH(req: NextApiRequest, res: NextApiResponse) {
-  const queryId = req.query.id as string;
-  const patchRequest = await axios.patch(`${baseURL}/${queryId}`, req.body);
-  return res.status(patchRequest.status).json(req.body);
+  try {
+    const queryId = req.query.id as string;
+    const patchRequest = await axios.patch(`${baseURL}/${queryId}`, req.body);
+    return res.status(patchRequest.status).json(req.body);
+  } catch (e) {
+    return catchError(res, e);
+  }
 }
 
 async function handlePUT(req: NextApiRequest, res: NextApiResponse) {
-  const queryId = req.query.id as string;
-  const putRequest = await axios.put(`${baseURL}/${queryId}`, req.body);
-  return res.status(putRequest.status).json(req.body);
+  try {
+    const queryId = req.query.id as string;
+    const putRequest = await axios.put(`${baseURL}/${queryId}`, req.body);
+    return res.status(putRequest.status).json(req.body);
+  } catch (e) {
+    return catchError(res, e);
+  }
 }
 
 async function handleDELETE(req: NextApiRequest, res: NextApiResponse) {
