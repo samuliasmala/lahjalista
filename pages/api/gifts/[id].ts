2fbaf75cb9e70ce9a9409803f38872e9caf34e94
Antti Asmala <antti.asmala@gmail.com>
2024-02-07 12:36:47 +0200

    Added a try/catch block

--- pages/api/gifts/[id].ts
+++ pages/api/gifts/[id].ts
@@ -1,14 +1,17 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== 'GET') {
-    return res
-      .status(405)
-      .send(
-        `${req.method} is not a valid method. Only GET requests are valid!`,
-      );
+  try {
+    if (req.method !== 'GET') {
+      return res
+        .status(405)
+        .send(
+          `${req.method} is not a valid method. Only GET requests are valid!`,
+        );
+    }
+    console.log(req.query, '[id].ts');
+    return res.status(200).send(req.query.id);
+  } catch (e) {
+    console.log('Error');
   }
-  console.log(req.query, '[id].ts');
-
-  return res.status(200).send(req.query.id);
 }
