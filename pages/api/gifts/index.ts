50163b9732d07651398ae9c25972f44da75b7356
Antti Asmala <antti.asmala@gmail.com>
2024-02-07 14:47:07 +0200

    Moved functions and variables a bit to make them more clear

--- pages/api/gifts/index.ts
+++ pages/api/gifts/index.ts
@@ -4,31 +4,18 @@ import { Gift } from '../..';
 
 const baseURL = 'http://localhost:3001/gifts';
 
-function errorFound(res: NextApiResponse, e: unknown) {
-  if (isAxiosError(e) && e.response?.status === 404) {
-    return res
-      .status(e.response.status)
-      .send('Lahjaa ei löytynyt palvelimelta!');
-  } else if (isAxiosError(e) && e.code === 'ECONNREFUSED') {
-    return res.status(500).send('Palvelin virhe!');
-  } else if (e instanceof Error) {
-    return res.status(500).send('Odottamaton virhe tapahtui!');
-  } else {
-    return res.status(500).send('Odottamaton virhe tapahtui!');
-  }
-}
+const HANDLERS: Record<
+  string,
+  (req: NextApiRequest, res: NextApiResponse) => Promise<void>
+> = {
+  GET: handleGET,
+} as const;
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
   try {
-    const HANDLERS: Record<
-      string,
-      (req: NextApiRequest, res: NextApiResponse) => Promise<void>
-    > = {
-      GET: handleGET,
-    } as const;
     const reqHandler = req.method !== undefined && HANDLERS[req.method];
     if (reqHandler) {
       await reqHandler(req, res);
@@ -45,4 +32,18 @@ export default async function handler(
 async function handleGET(req: NextApiRequest, res: NextApiResponse) {
   const giftRequest = await axios.get(`${baseURL}`);
   return res.status(giftRequest.status).json(giftRequest.data as Gift);
+}
+
+function errorFound(res: NextApiResponse, e: unknown) {
+  if (isAxiosError(e) && e.response?.status === 404) {
+    return res
+      .status(e.response.status)
+      .send('Lahjaa ei löytynyt palvelimelta!');
+  } else if (isAxiosError(e) && e.code === 'ECONNREFUSED') {
+    return res.status(500).send('Palvelin virhe!');
+  } else if (e instanceof Error) {
+    return res.status(500).send('Odottamaton virhe tapahtui!');
+  } else {
+    return res.status(500).send('Odottamaton virhe tapahtui!');
+  }
 }
