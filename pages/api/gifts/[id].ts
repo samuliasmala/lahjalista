fb91d72ef551df81d7530d849209d95acda43951
Antti Asmala <antti.asmala@gmail.com>
2024-02-07 12:35:52 +0200

    Added a check if the request is GET

--- pages/api/gifts/[id].ts
+++ pages/api/gifts/[id].ts
@@ -1,6 +1,14 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'GET') {
+    return res
+      .status(405)
+      .send(
+        `${req.method} is not a valid method. Only GET requests are valid!`,
+      );
+  }
   console.log(req.query, '[id].ts');
+
   return res.status(200).send(req.query.id);
 }
