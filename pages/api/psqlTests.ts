3d9f26ed30837fe40be214128b1dc97b0d9b2395
Antti Asmala <antti.asmala@gmail.com>
2024-02-08 12:20:08 +0200

    Testing getting the errors. Created a handler for errors. And created a new function called handleError

--- pages/api/psqlTests.ts
+++ pages/api/psqlTests.ts
@@ -1,36 +1,56 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from 'next';
 import pg from 'pg';
-import { Gift } from "..";
+import { Gift } from '..';
+import * as pgErrorHandlers from '~/utils/psqlFunctions';
 
 const pool = new pg.Pool({
-    host: "localhost",
-    port: 5432,
-    user: "postgres",
-    password: process.env["POSTGRESQL_PWD"],
-    database: "giftsDB",
-    connectionTimeoutMillis: 0,
-    idleTimeoutMillis: 10000,
-    max: 10,
-    allowExitOnIdle: false
+  host: 'localhost',
+  port: 5432,
+  user: 'postgres',
+  password: process.env['POSTGRESQL_PWD'],
+  database: 'giftDB',
+  connectionTimeoutMillis: 0,
+  idleTimeoutMillis: 10000,
+  max: 10,
+  allowExitOnIdle: false,
+});
 
-})
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method === 'GET') {
+    try {
+      const client = await pool.connect();
+      await client.query('SET search_path TO "public"'); // set Schema to gifts
+      const result = await client.query(`SELECT * FROM "gifts"`); // select all the columns inside table: gifts
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === "GET") {
-        try {
-            const client = await pool.connect()
-            await client.query('SET search_path TO "gifts"') // set Schema to gifts
-            const result = await client.query(`SELECT * FROM "gifts"`) // select all the columns inside table: gifts
-
-            return res.status(200).json(result.rows as Gift[])
-        }
-        catch (error) {
-            if (error instanceof Error) {
-                console.log(error)
-                return res.status(404).send("Virhe tapahtui!")
-            }
-            return res.status(404).send("Odottamaton virhe tapahtui!")
-        }
+      return res.status(200).json(result.rows as Gift[]);
+    } catch (e) {
+      handleError(e);
+      /*
+      if (e instanceof Error) {
+        handleError(e);
+        return res.status(404).send('Virhe tapahtui!');
+      }
+      */
+      //console.log(e);
+      return res.status(404).send('Odottamaton virhe tapahtui!');
     }
-    return res.status(404).send("Vain GET-request!")
+  }
+  return res.status(404).send('Vain GET-request!');
+}
+
+const HANDLER: Record<string, () => void> = {
+  '3D000': pgErrorHandlers.handle3D000,
+  '42P01': pgErrorHandlers.handle42P01,
+} as const;
+
+function handleError(e: unknown) {
+  if (e instanceof pg.DatabaseError) {
+    const codeHandler = e.code !== undefined && HANDLER[e.code];
+    if (codeHandler) {
+      codeHandler();
+    }
+  }
 }
