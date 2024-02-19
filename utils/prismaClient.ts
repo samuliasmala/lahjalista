47363cf2b1e7aab42511a104bf52e00ad30b0363
Antti Asmala <antti.asmala@gmail.com>
2024-02-19 16:27:19 +0200

    Created a global prisma client. Reference: https://www.prisma.io/docs/orm/more/help-and-troubleshooting/help-articles/nextjs-prisma-client-dev-practices#solution

--- utils/prismaClient.ts
+++ utils/prismaClient.ts
@@ -1,5 +1,15 @@
-/*
 import { PrismaClient } from '@prisma/client';
 
-const globalPrismaClient = new PrismaClient();
-*/
+function prismaClientSingleton() {
+  return new PrismaClient();
+}
+
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
+}
+
+const prisma = globalThis.prisma ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma;
