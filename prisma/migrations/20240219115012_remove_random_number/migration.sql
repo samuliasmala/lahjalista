8e870ab471610f4a1e771df2c043bea6694a8e7c
Antti Asmala <antti.asmala@gmail.com>
2024-02-19 13:52:07 +0200

    Did some tests with prisma

---
+++ prisma/migrations/20240219115012_remove_random_number/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `randomNumber` on the `Gift` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Gift" DROP COLUMN "randomNumber";
