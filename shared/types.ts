bd6803db77c14cd7acbba14d1d16e9a34574c050
Antti Asmala <antti.asmala@gmail.com>
2024-03-05 12:14:31 +0200

    Changed authorEmail to userUUID in Gift type

--- shared/types.ts
+++ shared/types.ts
@@ -1,6 +1,6 @@
 import { Prisma, Gift as PrismaGift } from '@prisma/client';
 
-export type Gift = Omit<PrismaGift, 'id' | 'authorEmail'>;
+export type Gift = Omit<PrismaGift, 'id' | 'userUUID'>;
 export type CreateGift = Omit<
   Prisma.GiftCreateInput,
   'uuid' | 'createdAt' | 'updatedAt'
