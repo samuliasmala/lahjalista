3205825bcab335f9befed27ec9e589f0c57d2724
Antti Asmala <antti.asmala@gmail.com>
2024-03-05 15:39:54 +0200

    Added an error if trying to send an invalid request body to server (in POST or PUT request)

--- backend/handleError.ts
+++ backend/handleError.ts
@@ -1,4 +1,7 @@
-import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import {
+  PrismaClientKnownRequestError,
+  PrismaClientValidationError,
+} from '@prisma/client/runtime/library';
 import { NextApiResponse } from 'next';
 
 export function handleError(res: NextApiResponse, e: unknown) {
@@ -6,6 +9,10 @@ export function handleError(res: NextApiResponse, e: unknown) {
     if (e.code === 'P2025') {
       return res.status(404).send('Gift was not found on the server!');
     }
+  }
+
+  if (e instanceof PrismaClientValidationError) {
+    return res.status(400).send('Invalid request body!');
   }
 
   return res.status(500).send('Server error!');
