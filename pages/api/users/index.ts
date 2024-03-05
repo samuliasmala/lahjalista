08d2f7e94bf935d479f541cd9affa900e465a931
Antti Asmala <antti.asmala@gmail.com>
2024-03-05 14:51:32 +0200

    Copied request handlers from 'gifts'

---
+++ pages/api/users/index.ts
@@ -0,0 +1,76 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { CreateGift, Gift } from '~/shared/types';
+import prisma from '~/prisma';
+
+const HANDLER: Record<
+  string,
+  (req: NextApiRequest, res: NextApiResponse) => Promise<void>
+> = {
+  GET: handleGET,
+  POST: handlePOST,
+};
+
+export default async function handlePrisma(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  try {
+    const reqHandler = req.method !== undefined && HANDLER[req.method];
+    if (reqHandler) {
+      await reqHandler(req, res);
+    } else {
+      return res
+        .status(405)
+        .send(
+          `${req.method} is not a valid method. Only GET and POST requests are valid!`,
+        );
+    }
+  } catch (e) {
+    return errorFound(res, e);
+  }
+}
+
+async function handleGET(req: NextApiRequest, res: NextApiResponse<Gift[]>) {
+  const gifts = await prisma.gift.findMany({
+    select: {
+      createdAt: true,
+      gift: true,
+      receiver: true,
+      updatedAt: true,
+      uuid: true,
+    },
+  });
+
+  return res.status(200).json(gifts);
+}
+
+async function handlePOST(req: NextApiRequest, res: NextApiResponse<Gift>) {
+  const giftData = req.body as CreateGift;
+  const addedGift = await prisma.gift.create({
+    data: {
+      gift: giftData.gift,
+      receiver: giftData.receiver,
+    },
+    select: {
+      createdAt: true,
+      gift: true,
+      receiver: true,
+      updatedAt: true,
+      uuid: true,
+    },
+  });
+
+  return res.status(200).json(addedGift);
+}
+
+export function errorFound(res: NextApiResponse, e: unknown) {
+  if (e instanceof Error) {
+    if (e.message.toLowerCase() === 'no gift found') {
+      return res.status(400).send('Gift was not found!');
+    }
+    if (e.cause === 'idError') return res.status(400).send('Invalid ID!');
+    return res.status(500).send('Server error!');
+  }
+
+  return res.status(500).send('Unexpected error occurred!');
+}
