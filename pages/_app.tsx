9147ece025ed4ad04330219a0093e05c2f347fca
Antti Asmala <antti.asmala@gmail.com>
2024-03-13 16:11:40 +0200

    Removed width=device-width. Didn't seem to have any impact

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, interactive-widget=resizes-content"
+          content="initial-scale=1.0, interactive-widget=resizes-content"
         />
       </Head>
       <Component {...pageProps} />
