570db07bd90f241b4c5bf90c026abe00665a50d2
Antti Asmala <antti.asmala@gmail.com>
2024-03-14 21:18:31 +0200

    Added the old Button component as a base. Added props

--- components/LoginAndSignOutButton.tsx
+++ components/LoginAndSignOutButton.tsx
@@ -1,7 +1,14 @@
 import { ButtonHTMLAttributes } from 'react';
+import { Button } from './Button';
 
 export function LoginAndSignOutButton({
   children,
   className,
   ...rest
-}: ButtonHTMLAttributes<HTMLButtonElement>) {}
+}: ButtonHTMLAttributes<HTMLButtonElement>) {
+  return (
+    <Button className={className} {...rest}>
+      {children}
+    </Button>
+  );
+}
