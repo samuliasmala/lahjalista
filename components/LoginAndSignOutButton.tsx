1589632645a45707304df37a16137810613cba2c
Antti Asmala <antti.asmala@gmail.com>
2024-03-15 10:39:18 +0200

    Login button now will show either sign in or sign out depending if user is logged in or not

--- components/LoginAndSignOutButton.tsx
+++ components/LoginAndSignOutButton.tsx
@@ -1,14 +1,29 @@
 import { ButtonHTMLAttributes } from 'react';
 import { Button } from './Button';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 export function LoginAndSignOutButton({
   children,
   className,
   ...rest
 }: ButtonHTMLAttributes<HTMLButtonElement>) {
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <>
+        <p className="text-3xl">Signed in as {session.user?.email}</p>
+        <Button className={className} {...rest}>
+          Sign out
+        </Button>
+      </>
+    );
+  }
   return (
-    <Button className={className} {...rest}>
-      {children}
-    </Button>
+    <>
+      <p className="text-3xl">Not signed in</p>
+      <Button className={className} {...rest}>
+        Sign in
+      </Button>
+    </>
   );
 }
