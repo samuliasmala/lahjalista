d268ae65d8575729493d255f1633b60e1c5a16b0
Antti Asmala <antti.asmala@gmail.com>
2024-03-19 18:35:04 +0200

    Added email regex pattern there and some info about it

--- utils/regexPatterns.ts
+++ utils/regexPatterns.ts
@@ -1,1 +1,6 @@
-
+/**
+ * This regex pattern is using the official standard: RFC5322. More details: https://www.ietf.org/rfc/rfc5322.txt
+ * Reference: https://emailregex.com/
+ */
+export const emailRegex =
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
