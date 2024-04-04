de68abf70da29f0fee57ff44a221c9410808a50c
Antti Asmala <antti.asmala@gmail.com>
2024-04-04 17:39:54 +0300

    Created a custom header check so backend can call backend APIs without middleware interfering. Most likely will be replaced / deleted, but it works for now in development stage

--- middleware.ts
+++ middleware.ts
@@ -2,12 +2,23 @@ import { verifyRequestOrigin } from 'lucia';
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
+const CUSTOM_HEADER = {
+  key: process.env.HEADER_KEY,
+  value: process.env.HEADER_VALUE,
+} as const;
+
 export async function middleware(req: NextRequest): Promise<NextResponse> {
   if (req.method === 'GET') {
     return NextResponse.next();
   }
   const originHeader = req.headers.get('Origin');
   const hostHeader = req.headers.get('Host');
+
+  if (CUSTOM_HEADER.key !== undefined && CUSTOM_HEADER.value !== undefined) {
+    if (req.headers.get(CUSTOM_HEADER.key) === CUSTOM_HEADER.value) {
+      return NextResponse.next();
+    }
+  }
 
   if (
     !originHeader ||
