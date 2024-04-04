7defc17e3a99c9068edb3e5f7342d4bf4068e9a5
Antti Asmala <antti.asmala@gmail.com>
2024-04-04 12:11:19 +0300

    Copied pages/login.tsx file from old login branch

---
+++ pages/login.tsx
@@ -0,0 +1,116 @@
+import { Inter } from 'next/font/google';
+import { useRouter } from 'next/router';
+import { FormEvent, useState } from 'react';
+import { Input } from '~/components/Input';
+import { LoginAndSignOutButton } from '~/components/LoginAndSignOutButton';
+import { TitleText } from '~/components/TitleText';
+import { signIn } from 'next-auth/react';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function Login() {
+  const [email, setEmail] = useState('john.doe@doemail.com');
+  const [password, setPassword] = useState('1');
+  const [isError, setIsError] = useState(false);
+  const [errorText, setErrorText] = useState('');
+
+  const router = useRouter();
+
+  function handleRegisterRedirect(
+    e: React.MouseEvent<HTMLSpanElement, MouseEvent>,
+  ) {
+    e.preventDefault();
+    router.push('/register').catch((e) => console.error(e));
+  }
+
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const loggingAttempt = await signIn('credentials', {
+      email: email,
+      password: password,
+      callbackUrl: '/',
+      redirect: false,
+    });
+    console.log(loggingAttempt);
+    if (loggingAttempt === undefined) {
+      return handleUndefinedLogging();
+    }
+
+    if (loggingAttempt.ok === false) {
+      if (loggingAttempt.error === 'CredentialsSignin') {
+        setErrorText('Virheellinen sähköposti tai salasana.');
+        setIsError(true);
+      }
+      return;
+    }
+
+    return await router.push('/');
+  }
+
+  function handleUndefinedLogging() {
+    setErrorText('Palvelin virhe!');
+    setIsError(true);
+  }
+
+  return (
+    <main className={`bg-white w-full max-w-full h-screen ${inter.className}`}>
+      <div className="h-screen w-screen bg-no-repeat bg-cover bg-center">
+        {isError ? (
+          <div className="pt-3 flex justify-center ">
+            <div className="max-w-sm text-center bg-red-100 border border-red-400 text-red-700 p-3 rounded [overflow-wrap:anywhere]">
+              {errorText}
+            </div>
+          </div>
+        ) : null}
+        <div className="w-full flex justify-center">
+          <div className="mt-5 flex flex-col">
+            <form onSubmit={(e) => void handleSubmit(e)}>
+              <TitleText className="text-center">Kirjaudu sisään</TitleText>
+              <div className="mt-5 flex flex-col">
+                <label>Sähköposti</label>
+                <Input
+                  onChange={(e) => setEmail(e.currentTarget.value)}
+                  value={email}
+                  className="border border-black pl-8 pr-8"
+                  autoComplete="off"
+                  type="text"
+                  placeholder="matti.meikalainen@email.com"
+                  name="username"
+                  spellCheck="false"
+                />
+              </div>
+              <div className="mt-5 flex flex-col">
+                <label>Salasana</label>
+                <Input
+                  onChange={(e) => setPassword(e.currentTarget.value)}
+                  value={password}
+                  className="border border-black"
+                  autoComplete="off"
+                  type="password"
+                  placeholder="************"
+                  name="username"
+                />
+              </div>
+              <div className="flex mt-3">
+                <label className="select-none cursor-pointer">
+                  <input type="checkbox" className="mr-2 cursor-pointer" />
+                  Muista minut
+                </label>
+              </div>
+              <LoginAndSignOutButton type="submit" />
+            </form>
+            <p className="mt-6 text-xs text-gray-600 select-none">
+              Sinulla ei ole vielä tunnuksia?{' '}
+              <span
+                className="underline cursor-pointer select-none hover:text-black"
+                onClick={(e) => handleRegisterRedirect(e)}
+              >
+                Luo tunnus
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
