c0cb21a6490f8eabc59117e64962da89780c693c
Antti Asmala <antti.asmala@gmail.com>
2024-04-04 14:34:43 +0300

    Copied an example of auth from lucia-auth's website: References: https://lucia-auth.com/getting-started/nextjs-pages and https://lucia-auth.com/tutorials/username-and-password/nextjs-pages

---
+++ backend/auth.ts
@@ -0,0 +1,31 @@
+import { PrismaAdapter } from '@lucia-auth/adapter-prisma';
+import { Lucia } from 'lucia';
+import prisma from '~/prisma';
+import { webcrypto } from 'crypto';
+import type { User } from '~/shared/types';
+
+globalThis.crypto = webcrypto as Crypto;
+
+const adapter = new PrismaAdapter(prisma.session, prisma.user);
+
+export const lucia = new Lucia(adapter, {
+  sessionCookie: {
+    attributes: {
+      secure: process.env.NODE_ENV === 'production',
+    },
+  },
+  getUserAttributes(attributes: Partial<User>) {
+    return {
+      email: attributes.email,
+    };
+  },
+});
+
+declare module 'lucia' {
+  interface Register {
+    Lucia: typeof lucia;
+    DatabaseUserAttributes: DatabaseUserAttributes;
+  }
+}
+
+type DatabaseUserAttributes = {} & User;
