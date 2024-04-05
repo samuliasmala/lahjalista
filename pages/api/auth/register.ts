c871d5b8f4e95f30e28924471ef0570067e539b7
Antti Asmala <antti.asmala@gmail.com>
2024-04-05 20:29:29 +0300

    Moved isPasswordValid etc into backend/utils

--- pages/api/auth/register.ts
+++ pages/api/auth/register.ts
@@ -3,9 +3,14 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { lucia } from '~/backend/auth';
 import { handleError } from '~/backend/handleError';
 import { HttpError } from '~/backend/HttpError';
+import {
+  isFirstNameValid,
+  isLastNameValid,
+  isPasswordValid,
+} from '~/backend/utils';
+import { isEmailValid } from '~/backend/utils';
 import { CUSTOM_HEADER } from '~/middleware';
 import type { CreateUser, User } from '~/shared/types';
-import { emailRegex, passwordRegex } from '~/utils/regexPatterns';
 
 export default async function handler(
   req: NextApiRequest,
@@ -68,47 +73,4 @@ export default async function handler(
   } catch (e) {
     return handleError(res, e);
   }
-}
-
-function isFirstNameValid(firstName: string): boolean {
-  if (firstName.length <= 0) {
-    throw new HttpError('First name is mandatory!', 400);
-  }
-  return true;
-}
-
-function isLastNameValid(lastName: string): boolean {
-  if (lastName.length <= 0) {
-    throw new HttpError('Last name is mandatory!', 400);
-  }
-  return true;
-}
-
-export function isEmailValid(email: string): boolean {
-  if (email.length <= 0) {
-    throw new HttpError('Email is mandatory!', 400);
-  }
-  // this should check with regex that there cannot be multiple dots etc
-  const checkedEmailAddress = email.toLowerCase().match(emailRegex);
-
-  if (!checkedEmailAddress) {
-    throw new HttpError('Invalid email!', 400);
-  }
-
-  // email is ready to be used
-  return true;
-}
-
-function isPasswordValid(password: string): boolean {
-  if (password.length <= 0) {
-    throw new HttpError('Password is mandatory!', 400);
-  }
-  // TLDR: 8 merkkiä pitkä, maksimissaan 128, vähintään 1 numero, 1 pieni ja iso kirjain sekä yksi erikoismerkki
-  const checkedPassword = password.match(passwordRegex);
-
-  if (!checkedPassword) {
-    throw new HttpError('Invalid password!', 400);
-  }
-
-  return true;
 }
