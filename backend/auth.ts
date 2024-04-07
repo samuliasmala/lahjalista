2fe00c41547d99996c4430c61b4e36012e0fed29
Antti Asmala <antti.asmala@gmail.com>
2024-04-07 16:30:22 +0300

    Created a function base for session handler

--- backend/auth.ts
+++ backend/auth.ts
@@ -1,7 +1,9 @@
 import { PrismaAdapter } from '@lucia-auth/adapter-prisma';
+import type { IncomingMessage, ServerResponse } from 'http';
 import { Lucia } from 'lucia';
 import prisma from '~/prisma';
 import type { CreateSession, User } from '~/shared/types';
+import type { Session, User as LuciaUser } from 'lucia';
 
 const adapter = new PrismaAdapter(prisma.session, prisma.user);
 
@@ -25,7 +27,35 @@ declare module 'lucia' {
     DatabaseSessionAttributes: DatabaseSessionAttributes;
   }
 }
-
 interface DatabaseUserAttributes extends User {}
-
 interface DatabaseSessionAttributes extends CreateSession {}
+
+export async function validateRequest(
+  req: IncomingMessage,
+  res: ServerResponse,
+): Promise<
+  { user: LuciaUser; session: Session } | { user: null; session: null }
+> {
+  const sessionId = lucia.readSessionCookie(req.headers.cookie ?? '');
+  if (!sessionId) {
+    return {
+      user: null,
+      session: null,
+    };
+  }
+  const result = await lucia.validateSession(sessionId);
+  if (result.session && result.session.fresh) {
+    res.appendHeader(
+      'Set-Cookie',
+      lucia.createSessionCookie(result.session.id).serialize(),
+    );
+  }
+  if (!result.session) {
+    res.appendHeader(
+      'Set-Cookie',
+      lucia.createBlankSessionCookie().serialize(),
+    );
+  }
+
+  return result;
+}
