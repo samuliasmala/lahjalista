e33bafd6b13bc681c471c69ee4ea1cde7cf2f0cc
Antti Asmala <antti.asmala@gmail.com>
2024-04-08 14:09:38 +0300

    Made error P2002 to use same logic as P2025 error when creating an error message

--- backend/handleError.ts
+++ backend/handleError.ts
@@ -17,12 +17,11 @@ export function handleError(res: NextApiResponse, e: unknown) {
       return res.status(404).send(`${modelName} was not found on the server!`);
     }
     if (e.code === 'P2002') {
-      if (Array.isArray(e.meta?.target) === true) {
-        const targetName =
-          typeof e.meta?.target[0] === 'string' ? e.meta?.target[0] : 'Record';
-        return res.status(400).send(`${targetName} was not unique!`);
-      }
-      return res.status(400).send('Record was not unique!');
+      const targetName =
+        Array.isArray(e.meta?.target) && typeof e.meta.target[0] === 'string'
+          ? e.meta.target[0]
+          : 'Record';
+      return res.status(400).send(`${targetName} was not unique!`);
     }
   }
 
