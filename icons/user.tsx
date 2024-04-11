2500743697863b6fde5c1ba35038387e3fd9827c
Antti Asmala <antti.asmala@gmail.com>
2024-04-11 22:41:57 +0300

    Created a new SVG user and converted it to .tsx file"

---
+++ icons/user.tsx
@@ -0,0 +1,19 @@
+import type { SVGProps } from 'react';
+const SvgUser = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth={1.5}
+    className="user_svg__w-6 user_svg__h-6"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0M4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.9 17.9 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632"
+    />
+  </svg>
+);
+export default SvgUser;
