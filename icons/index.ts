fad4a56e88821c8beb53508056a46d2dc9581c6c
Antti Asmala <antti.asmala@gmail.com>
2024-04-12 22:43:30 +0300

    Added credits for the SVG image and converted a .tsx file of it

--- icons/index.ts
+++ icons/index.ts
@@ -1,4 +1,5 @@
 export { default as AcceptButtonIcon } from './accept_button_icon';
+export { default as ArrowRightStartOnRectangle } from './arrow_right_start_on_rectangle';
 export { default as DeclineButtonIcon } from './decline_button_icon';
 export { default as EyeOpen } from './eye_open';
 export { default as EyeSlash } from './eye_slash';
