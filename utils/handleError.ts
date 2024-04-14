70901bad689eda4ef018d56923301b2b05f61553
Antti Asmala <antti.asmala@gmail.com>
2024-04-14 21:03:58 +0300

    Created a function for login errors and added login error to known front end errors

--- utils/handleError.ts
+++ utils/handleError.ts
@@ -23,10 +23,14 @@ export function handleGiftError(e: unknown) {
   }
 }
 
-type KnownFrontEndErrorTexts = 'email was not unique!' | string;
+type KnownFrontEndErrorTexts =
+  | 'email was not unique!'
+  | 'invalid credentials!'
+  | string;
 
-const USER_ERROR_HANDLER: Record<KnownFrontEndErrorTexts, string> = {
+const FRONT_END_HANDLER: Record<KnownFrontEndErrorTexts, string> = {
   'email was not unique!': 'Sähköposti on jo käytössä',
+  'invalid credentials!': 'Sähköposti tai salasana on virheellinen!',
 };
 
 export function handleRegisterError(e: unknown) {
@@ -34,13 +38,35 @@ export function handleRegisterError(e: unknown) {
     const responseText = e.response.data.toLowerCase();
     if (
       typeof e.response.data === 'string' &&
-      USER_ERROR_HANDLER[responseText]
+      FRONT_END_HANDLER[responseText]
     ) {
-      console.error(USER_ERROR_HANDLER[responseText]);
-      return USER_ERROR_HANDLER[responseText];
+      console.error(FRONT_END_HANDLER[responseText]);
+      return FRONT_END_HANDLER[responseText];
     } else {
       console.error('Unexpected error occured!');
       console.error('Unexpected error: ', e.response.data);
+    }
+  } else if (e instanceof Error) {
+    console.error(e.message);
+  } else {
+    console.error(e);
+  }
+  return '';
+}
+
+export function handleLoginError(e: unknown) {
+  if (isAxiosError(e) && e.response?.status === 400) {
+    const responseText = e.response.data.toLowerCase();
+    if (
+      typeof e.response.data === 'string' &&
+      FRONT_END_HANDLER[responseText]
+    ) {
+      console.error(FRONT_END_HANDLER[responseText]);
+      return FRONT_END_HANDLER[responseText];
+    } else {
+      console.error('Unexpected error occured!');
+      console.error('Unexpected error: ', e.response.data);
+      return 'Palvelin virhe!';
     }
   } else if (e instanceof Error) {
     console.error(e.message);
