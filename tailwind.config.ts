c0876b1a0107a29191c493192f9e2ae50c74b801
Antti Asmala <antti.asmala@gmail.com>
2024-04-15 17:25:54 +0300

    Added hoverOnlyWhenSupported to be true to make hover elements work as intended when using mobile

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,9 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
