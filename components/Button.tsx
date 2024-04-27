7388ad7f108e446edb29843257bf72c36ba45fae
Antti Asmala <antti.asmala@gmail.com>
2024-04-27 23:22:02 +0300

    Added rounding to the Button

--- components/Button.tsx
+++ components/Button.tsx
@@ -9,7 +9,7 @@ export function Button({
   return (
     <button
       className={twMerge(
-        'w-full text-s mt-6 p-2 text-white border bg-black hover:text-gray-500',
+        'w-full text-s mt-6 p-2 text-white border bg-black hover:text-gray-500 rounded-md',
         className,
       )}
       {...rest}
