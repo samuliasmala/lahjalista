318e45cc9f449aa4ae8edc66ca2dbc07ea362fdd
Antti Asmala <antti.asmala@gmail.com>
2024-05-10 20:10:08 +0300

    Removed customDestination parameter. Removed an as statement

--- utils/getServerSideProps.ts
+++ utils/getServerSideProps.ts
@@ -4,14 +4,13 @@ import { User } from '~/shared/types';
 
 export async function getServerSideProps(
   context: GetServerSidePropsContext,
-  customDestination?: string,
 ): Promise<GetServerSidePropsResult<{ user: User }>> {
   const cookieData = await validateRequest(context.req, context.res);
   if (!cookieData.user) {
     return {
       redirect: {
         permanent: false,
-        destination: customDestination || '/login',
+        destination: '/login',
       },
     };
   }
