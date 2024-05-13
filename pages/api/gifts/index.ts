508d84a81534d38171c78765303f33ebd96c621d
Antti Asmala <antti.asmala@gmail.com>
2024-05-13 15:20:17 +0300

    Added a check to only return the gifts the requester has an access to. Added a new parameter userData that contains requester's data (id, uuid, etc...)

--- pages/api/gifts/index.ts
+++ pages/api/gifts/index.ts
@@ -3,10 +3,16 @@ import { CreateGift, Gift } from '~/shared/types';
 import prisma from '~/prisma';
 import { handleError } from '~/backend/handleError';
 import { HttpError } from '~/backend/HttpError';
+import { validateRequest } from '~/backend/auth';
+import { User as LuciaUser } from 'lucia';
 
 const HANDLER: Record<
   string,
-  (req: NextApiRequest, res: NextApiResponse) => Promise<void>
+  (
+    req: NextApiRequest,
+    res: NextApiResponse,
+    userData: LuciaUser,
+  ) => Promise<void>
 > = {
   GET: handleGET,
   POST: handlePOST,
@@ -17,9 +23,13 @@ export default async function handlePrisma(
   res: NextApiResponse,
 ) {
   try {
+    const validationRequest = await validateRequest(req, res);
+    if (!validationRequest.session || !validationRequest.user) {
+      throw new HttpError('You are unauthorized!', 401);
+    }
     const reqHandler = req.method !== undefined && HANDLER[req.method];
     if (reqHandler) {
-      await reqHandler(req, res);
+      await reqHandler(req, res, validationRequest.user);
     } else {
       throw new HttpError(
         `${req.method} is not a valid method. Only GET and POST requests are valid!`,
@@ -31,7 +41,11 @@ export default async function handlePrisma(
   }
 }
 
-async function handleGET(req: NextApiRequest, res: NextApiResponse<Gift[]>) {
+async function handleGET(
+  req: NextApiRequest,
+  res: NextApiResponse<Gift[]>,
+  userData: LuciaUser,
+) {
   const gifts = await prisma.gift.findMany({
     select: {
       createdAt: true,
@@ -40,17 +54,29 @@ async function handleGET(req: NextApiRequest, res: NextApiResponse<Gift[]>) {
       updatedAt: true,
       uuid: true,
     },
+    where: {
+      userUUID: userData.uuid,
+    },
   });
 
   return res.status(200).json(gifts);
 }
 
-async function handlePOST(req: NextApiRequest, res: NextApiResponse<Gift>) {
+async function handlePOST(
+  req: NextApiRequest,
+  res: NextApiResponse<Gift>,
+  userData: LuciaUser,
+) {
   const giftData = req.body as CreateGift;
   const addedGift = await prisma.gift.create({
     data: {
       gift: giftData.gift,
       receiver: giftData.receiver,
+      user: {
+        connect: {
+          uuid: userData.uuid,
+        },
+      },
     },
     select: {
       createdAt: true,
