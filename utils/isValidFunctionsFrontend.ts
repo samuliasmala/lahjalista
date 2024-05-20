1f5a227aff210cadf414a41a02fe2d30ca18a726
Antti Asmala <antti.asmala@gmail.com>
2024-05-20 19:25:16 +0300

    Changed complitely how the Zod checks are done in utils/isValidFunctionsFrontend.ts

--- utils/isValidFunctionsFrontend.ts
+++ utils/isValidFunctionsFrontend.ts
@@ -1,53 +1,40 @@
 import { z } from 'zod';
 import { emailRegex, passwordRegex } from '../shared/regexPatterns';
 
-export type ValidErrorFound = 'lengthError' | 'regexError';
-
-export function isEmailValid(email: string): ValidErrorFound | true {
-  if (email.length <= 0) {
-    return 'lengthError';
-  }
-  // this should check with regex that there cannot be multiple dots etc
-  const checkedEmailAddress = email.toLowerCase().match(emailRegex);
-
-  if (!checkedEmailAddress) {
-    return 'regexError';
-  }
-
-  // email is ready to be used
-  return true;
-}
-
-export function isFirstNameValid(firstName: string): boolean {
-  z.string().min(1).max(128).parse(firstName);
-  return true;
-}
-
-export function isLastNameValid(lastName: string): boolean {
-  z.string().min(1).max(128).parse(lastName);
-  return true;
-}
-
-export function isPasswordValid(password: string): ValidErrorFound | true {
+export function isFirstNameValid(firstName: string) {
   const zodCheck = z
     .string()
-    .min(1)
-    .max(128)
-    .regex(passwordRegex)
+    .min(1, { message: 'too_small' })
+    .max(128, { message: 'too_big' })
+    .safeParse(firstName);
+  return zodCheck.success ? zodCheck.success : zodCheck.error.issues[0];
+}
+
+export function isLastNameValid(lastName: string) {
+  const zodCheck = z
+    .string()
+    .min(1, { message: 'too_small' })
+    .max(128, { message: 'too_big' })
+    .safeParse(lastName);
+  return zodCheck.success ? zodCheck.success : zodCheck.error.issues[0];
+}
+
+export function isEmailValid(email: string) {
+  const zodCheck = z
+    .string()
+    .min(1, { message: 'too_small' })
+    .max(128, { message: 'too_big' })
+    .regex(emailRegex, { message: 'regex' })
+    .safeParse(email.toLowerCase());
+  return zodCheck.success ? zodCheck.success : zodCheck.error.issues[0];
+}
+
+export function isPasswordValid(password: string) {
+  const zodCheck = z
+    .string()
+    .min(1, { message: 'too_small' })
+    .max(128, { message: 'too_big' })
+    .regex(passwordRegex, { message: 'regex' })
     .safeParse(password);
-  if (!zodCheck.success) {
-    console.log(zodCheck.error);
-    const isTooSmallFound = zodCheck.error.issues.find(
-      (error) => error.code === 'too_small',
-    );
-
-    if (isTooSmallFound) return 'lengthError';
-
-    const isRegexErrorFound = zodCheck.error.issues.find(
-      (error) => error.code === 'invalid_string',
-    );
-
-    if (isRegexErrorFound) return 'regexError';
-  }
-  return true;
+  return zodCheck.success ? zodCheck.success : zodCheck.error.issues[0];
 }
