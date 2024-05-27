5d0fe91438aa7f75788ae06f60802ce2b477eb8d
Antti Asmala <antti.asmala@gmail.com>
2024-05-27 20:08:36 +0300

    Changed email validation to be a Zod check instead of a function that checks. Removed password length check

--- pages/login.tsx
+++ pages/login.tsx
@@ -9,9 +9,9 @@ import { Input } from '~/components/Input';
 import { TitleText } from '~/components/TitleText';
 import SvgEyeOpen from '~/icons/eye_open';
 import SvgEyeSlash from '~/icons/eye_slash';
-import { isEmailValid } from '~/shared/isValidFunctions';
 import { UserLoginDetails } from '~/shared/types';
 import { handleAuthErrors } from '~/utils/handleError';
+import { emailZodCheck } from '~/utils/zodChecks';
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   const cookieData = await validateRequest(context.req, context.res);
@@ -40,20 +40,18 @@ export default function Login() {
   async function handleSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
     try {
-      if (isEmailValid(email)) {
-        if (password.length <= 0) {
-          return setErrorText('Salasana-kenttä ei voi olla tyhjä!');
-        }
-        const loginCredentials: UserLoginDetails = {
-          email: email,
-          password: password,
-          rememberMe: rememberMe,
-        };
-        await axios.post('/api/auth/login', loginCredentials);
-        await router.push('/');
-      } else {
-        setErrorText('Sähköposti ei ole sääntöjen mukainen!');
+      const emailValidation = emailZodCheck.safeParse(email);
+      if (emailValidation.success === false) {
+        setErrorText(emailValidation.error.format()._errors[0] || '');
+        return;
       }
+      const loginCredentials: UserLoginDetails = {
+        email: email,
+        password: password,
+        rememberMe: rememberMe,
+      };
+      await axios.post('/api/auth/login', loginCredentials);
+      await router.push('/');
     } catch (e) {
       console.error(e);
       setErrorText(handleAuthErrors(e));
