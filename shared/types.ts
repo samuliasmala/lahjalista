fec3b8b73e8807586b52199f46569a5d05990cc0
Antti Asmala <antti.asmala@gmail.com>
2024-05-27 20:12:46 +0300

    Re-wrote schemas

--- shared/types.ts
+++ shared/types.ts
@@ -3,9 +3,9 @@ import {
   createGiftSchema,
   createSessionSchema,
   createUserSchema,
+  getUserSchema,
   giftSchema,
   userLoginDetailsSchema,
-  userSchema,
 } from './zodSchemas';
 
 export type {
@@ -17,7 +17,8 @@ export type {
 export type Gift = z.infer<typeof giftSchema>;
 export type CreateGift = z.infer<typeof createGiftSchema>;
 
-export type User = z.infer<typeof userSchema>;
+// CHECK THIS
+export type User = z.infer<typeof getUserSchema>;
 export type CreateUser = z.infer<typeof createUserSchema>;
 
 export type UserLoginDetails = z.infer<typeof userLoginDetailsSchema>;
--- shared/zodSchemas.ts
+++ shared/zodSchemas.ts
@@ -18,33 +18,36 @@ export const createGiftSchema = z.object({
 export const updateGiftSchema = createGiftSchema;
 
 export const userSchema = z.object({
+  email: z.string().min(1).max(128).regex(emailRegex).toLowerCase(),
+  firstName: z.string().min(1).max(128),
+  lastName: z.string().min(1).max(128),
+});
+
+export const getUserSchema = userSchema.extend({
   createdAt: z.date(),
   updatedAt: z.date(),
   uuid: z.string(),
-  email: z.string().min(1).max(128).regex(emailRegex),
-  firstName: z.string().min(1).max(128),
-  lastName: z.string().min(1).max(128),
 });
 
-export const createUserSchema = z.object({
-  email: z.string().min(1).max(128).regex(emailRegex),
-  firstName: z.string().min(1).max(128),
-  lastName: z.string().min(1).max(128),
+export const createUserSchema = userSchema.extend({
   password: z.string().min(1).max(128).regex(passwordRegex),
-  Session: z.custom<Prisma.SessionCreateNestedOneWithoutUserInput>().optional(),
 });
 
-export const updateUserSchema = z.object({
+export const updateUserSchema = userSchema;
+
+// CHECK THIS
+export const userLoginDetailsSchema = userSchema
+  .pick({ email: true })
+  .merge(createUserSchema.pick({ password: true }))
+  .extend({ rememberMe: z.boolean() });
+
+/*
+  export const userLoginDetailsSchema = z.object({
   email: z.string().min(1).max(128).regex(emailRegex),
-  firstName: z.string().min(1).max(128),
-  lastName: z.string().min(1).max(128),
-});
-
-export const userLoginDetailsSchema = z.object({
-  email: z.string().min(1).max(100).regex(emailRegex),
-  password: z.string().min(1).max(100).regex(passwordRegex),
+  password: z.string().min(1).max(128).regex(passwordRegex),
   rememberMe: z.boolean(),
 });
+*/
 
 export const createSessionSchema = z.object({
   createdAt: z.date(),
