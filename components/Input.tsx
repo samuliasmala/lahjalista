6faccb5eedec99d68d217fbe7803f190ae9ea10b
Antti Asmala <antti.asmala@gmail.com>
2024-06-17 21:11:35 +0300

    Changed border color and hovering color in components/Input.tsx

--- components/Input.tsx
+++ components/Input.tsx
@@ -8,7 +8,7 @@ export function Input({
   return (
     <input
       className={twMerge(
-        'ps-1 pt-3 pb-3 border border-black hover:bg-gray-100 rounded-md',
+        'ps-1 pt-3 pb-3 border border-lines hover:bg-primaryLight rounded-md',
         className,
       )}
       {...rest}
