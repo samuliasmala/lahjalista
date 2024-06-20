9b681f5795f6402c523ec6afc0a9b710067af41b
Antti Asmala <antti.asmala@gmail.com>
2024-06-20 11:41:36 +0300

    Copied Password styling from pages/login.tsx

--- pages/register.tsx
+++ pages/register.tsx
@@ -135,7 +135,7 @@ export default function Register() {
                 <ErrorParagraph errorText={errors.email} />
 
                 <Label className="pt-5">Salasana</Label>
-                <div className="flex rounded-md outline outline-1 border-black hover:bg-gray-100 has-[input:focus]:outline-2 has-[input:focus]:rounded group/password">
+                <div className="flex rounded-md outline outline-1 border-black bg-white hover:bg-primaryLight has-[input:focus]:outline-2 has-[input:focus]:rounded group/password">
                   <Input
                     value={formData.password}
                     onChange={(e) => {
@@ -144,15 +144,15 @@ export default function Register() {
                         password: e.currentTarget.value,
                       });
                     }}
-                    className="pl-1 pt-3 pb-3 border-0 outline-none group-hover/password:bg-gray-100"
+                    className="pl-1 pt-3 pb-3 border-0 outline-none group-hover/password:bg-primaryLight"
                     autoComplete="off"
                     type={showPassword ? 'text' : 'password'}
                     placeholder="************"
                     name="password"
                   />
-                  <div className="group-hover/password:bg-gray-100 hover:bg-white bg-white flex items-center ">
+                  <div className="group-hover/password:bg-primaryLight hover:bg-white bg-white flex items-center has-[input:focus]:outline-2 has-[input:focus]:rounded rounded-md">
                     <SvgEye
-                      className="w-8 h-8 cursor-pointer p-0 hover:stroke-yellow-600 "
+                      className="w-8 h-8 cursor-pointer p-0 hover:stroke-primary text-lines"
                       onClick={() => {
                         setShowPassword((prevValue) => !prevValue);
                       }}
