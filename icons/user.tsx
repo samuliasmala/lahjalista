52f4ec684ea974ea6bc7895c6bb4d8310a854d3f
Antti Asmala <antti.asmala@gmail.com>
2024-06-24 15:04:35 +0300

    Updated user icon to match that is in Figma

--- icons/user.tsx
+++ icons/user.tsx
@@ -3,16 +3,15 @@ const SvgUser = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
-    stroke="currentColor"
-    strokeWidth={1.5}
-    className="user_svg__w-6 user_svg__h-6"
-    viewBox="0 0 24 24"
+    viewBox="0 0 18 20"
     {...props}
   >
     <path
+      stroke="#404040"
       strokeLinecap="round"
       strokeLinejoin="round"
-      d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0M4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.9 17.9 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632"
+      strokeWidth={2}
+      d="M17 19c0-1.396 0-2.093-.172-2.661a4 4 0 0 0-2.667-2.667c-.568-.172-1.265-.172-2.661-.172h-5c-1.396 0-2.093 0-2.661.172a4 4 0 0 0-2.667 2.667C1 16.907 1 17.604 1 19M13.5 5.5a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0"
     />
   </svg>
 );
