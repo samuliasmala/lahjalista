7f98a6f212ae7344eb9dd04d11a016501a22ab17
Antti Asmala <antti.asmala@gmail.com>
2024-07-31 16:24:55 +0300

    Added a custom className 'strokeClassName' to have access to give custom class attributes to path value as well. Before it was only possible to give them to the main <svg> element

--- icons/user.tsx
+++ icons/user.tsx
@@ -1,6 +1,13 @@
 import type { SVGProps } from 'react';
 
-const SvgUser = (props: SVGProps<SVGSVGElement>) => (
+type SVGUser = SVGProps<SVGSVGElement> & {
+  strokeClassName?: string;
+};
+
+const SvgUser = ({
+  strokeClassName,
+  ...props
+}: SVGUser) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
@@ -12,6 +19,7 @@ const SvgUser = (props: SVGProps<SVGSVGElement>) => (
       strokeLinecap="round"
       strokeLinejoin="round"
       strokeWidth={2}
+      className={strokeClassName}
       d="M17 19c0-1.396 0-2.093-.172-2.661a4 4 0 0 0-2.667-2.667c-.568-.172-1.265-.172-2.661-.172h-5c-1.396 0-2.093 0-2.661.172a4 4 0 0 0-2.667 2.667C1 16.907 1 17.604 1 19M13.5 5.5a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0"
     />
   </svg>
