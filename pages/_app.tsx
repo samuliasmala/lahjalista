c8ec6a718e80f2b2ba4db48bc4029ee767ee0a31
Antti Asmala <antti.asmala@gmail.com>
2024-08-29 12:42:04 +0300

    Added the ToastContainer in pages/_app.tsx. This might get changed in the future

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,23 @@ import { Inter } from 'next/font/google';
 import '~/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { Bounce, ToastContainer } from 'react-toastify';
+import 'react-toastify/ReactToastify.css';
+import { isPhoneUser } from '~/utils/isPhoneUser';
+import { useEffect, useState } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
+// CHECK THIS, onko tässä tiedostossa mitään järkeä?
+
 export default function App({ Component, pageProps }: AppProps) {
+  const [isUserPhoneUser, setIsUserPhoneUser] = useState(false);
+  useEffect(() => {
+    if (window) {
+      setIsUserPhoneUser(isPhoneUser(window));
+    }
+  }, []);
+
   return (
     <div className={`${inter.className}`}>
       <Head>
@@ -14,6 +27,20 @@ export default function App({ Component, pageProps }: AppProps) {
           content="initial-scale=1.0, interactive-widget=resizes-visual"
         />
       </Head>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Bounce}
+        limit={isUserPhoneUser ? 1 : 3}
+      />
       <Component {...pageProps} />
     </div>
   );
