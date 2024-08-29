865086c1a6ebab2e6cea5fc21b2dc6921812bb61
Antti Asmala <antti.asmala@gmail.com>
2024-08-29 12:42:34 +0300

    Keeping the isPhoneUser function still in its own function. Most likely will move this to pages/_app.tsx file

--- utils/isPhoneUser.ts
+++ utils/isPhoneUser.ts
@@ -1,10 +1,13 @@
 // CHECK THIS, onko tässä mitään järkeä?
 
-export function isPhoneUser(window: Window & typeof globalThis): boolean {
-  const screenWidth = window.screen.width;
-  const screenHeight = window.screen.height;
-  if (screenWidth < 768 || screenHeight < 768) {
-    return true;
+export function isPhoneUser(window?: Window & typeof globalThis): boolean {
+  if (window) {
+    const screenWidth = window.screen.width;
+    const screenHeight = window.screen.height;
+    if (screenWidth < 768 || screenHeight < 768) {
+      return true;
+    }
+    return false;
   }
   return false;
 }
