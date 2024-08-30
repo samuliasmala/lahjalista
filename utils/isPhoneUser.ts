790325d94d3863ba3a94b2f52b084e26a843a782
Antti Asmala <antti.asmala@gmail.com>
2024-08-30 13:40:29 +0300

    Added a new comment about this function so I remember to check while doing a PR

--- utils/isPhoneUser.ts
+++ utils/isPhoneUser.ts
@@ -1,5 +1,7 @@
 // CHECK THIS, onko tässä mitään järkeä?
 
+// Ei taida olla tarpeellinen, jos tämä tarkistus tehdään ainoastaan _app.tsx-tiedostossa. Turhaan on oma tiedostonsa tälle
+
 export function isPhoneUser(window?: Window & typeof globalThis): boolean {
   if (window) {
     const screenWidth = window.screen.width;
