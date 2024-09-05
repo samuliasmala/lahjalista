e0edbe4a872049839659c61edfefb507148e8dd4
Antti Asmala <antti.asmala@gmail.com>
2024-09-05 10:49:25 +0300

    Created a new migration of current database. Couple column names were changed so had to do it again

--- prisma/migrations/20240612133919_feedback_and_feedback_session_model/migration.sql
+++ prisma/migrations/20240612133919_feedback_and_feedback_session_model/migration.sql
@@ -1,31 +1,31 @@
 -- CreateTable
 CREATE TABLE "Feedback" (
-    "id" SERIAL NOT NULL,
-    "uuid" TEXT NOT NULL,
+    "feedbackID" SERIAL NOT NULL,
+    "feedbackUUID" TEXT NOT NULL,
     "feedbackText" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "Feedback_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Feedback_pkey" PRIMARY KEY ("feedbackID")
 );
 
 -- CreateTable
 CREATE TABLE "FeedbackSession" (
-    "id" SERIAL NOT NULL,
-    "uuid" TEXT NOT NULL,
+    "feedbackSessionID" SERIAL NOT NULL,
+    "feedbackSessionUUID" TEXT NOT NULL,
     "userUUID" TEXT NOT NULL,
     "expiresAt" TIMESTAMP(3) NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "FeedbackSession_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "FeedbackSession_pkey" PRIMARY KEY ("feedbackSessionID")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Feedback_uuid_key" ON "Feedback"("uuid");
+CREATE UNIQUE INDEX "Feedback_feedbackUUID_key" ON "Feedback"("feedbackUUID");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "FeedbackSession_uuid_key" ON "FeedbackSession"("uuid");
+CREATE UNIQUE INDEX "FeedbackSession_feedbackSessionUUID_key" ON "FeedbackSession"("feedbackSessionUUID");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "FeedbackSession_userUUID_key" ON "FeedbackSession"("userUUID");
