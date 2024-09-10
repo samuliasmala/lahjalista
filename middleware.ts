df1e1a18c036165a8a02a8d1d816ad011e8b9a78
Antti Asmala <antti.asmala@gmail.com>
2024-09-10 18:11:03 +0300

    Changed the way how Middleware works

--- middleware.ts
+++ middleware.ts
@@ -4,24 +4,17 @@ import type { NextRequest } from 'next/server';
 
 //eslint-disable-next-line @typescript-eslint/require-await
 export async function middleware(req: NextRequest): Promise<NextResponse> {
-  if (process.env.NODE_ENV == 'production') {
-    const originHeader = req.headers.get('Origin');
-    const hostHeader = req.headers.get('Host');
-    if (
-      !originHeader ||
-      !hostHeader ||
-      !verifyRequestOrigin(originHeader, [hostHeader])
-    ) {
-      const headerErrors = [];
-      !originHeader ? headerErrors.push('Origin header') : '';
-      !hostHeader ? headerErrors.push('Host header') : '';
-      !verifyRequestOrigin(originHeader || '', [hostHeader || ''])
-        ? headerErrors.push('Request origin header')
-        : '';
-      return new NextResponse(
-        `Middleware error!\n\nError: ${headerErrors[0]} was invalid!`,
-        { status: 403 },
-      );
+  if (process.env.NODE_ENV === 'production') {
+    try {
+      const originHeader = req.headers.get('Origin');
+      const hostHeader = req.headers.get('Host');
+      if (!originHeader) throw new Error('Origin header was not found!');
+      if (!hostHeader) throw new Error('Host header was not found!');
+      if (!verifyRequestOrigin(originHeader, [hostHeader]))
+        throw new Error('Request origin verification failed!');
+    } catch (e) {
+      console.error('Middleware error:', e instanceof Error ? e.message : '');
+      return new NextResponse('Middleware error!', { status: 403 });
     }
   }
   return NextResponse.next();
