6c717b9325ba950fdfab8a4b09876717999a7621
Antti Asmala <antti.asmala@gmail.com>
2024-09-27 10:00:25 +0300

    Make POST request handler and make functions to handle the appending

--- pages/api/google/index.ts
+++ pages/api/google/index.ts
@@ -1,32 +1,20 @@
-import { google } from 'googleapis';
-import { readFileSync } from 'fs';
 import { NextApiRequest, NextApiResponse } from 'next';
 import {
   GoogleApiAuthentication,
   GoogleApiSheets,
 } from '~/backend/googleAPI/GoogleApi';
 import { z } from 'zod';
-
-type GoogleAccount = {
-  type: string;
-  project_id: string;
-  private_key_id: string;
-  private_keys: string;
-  client_email: string;
-  client_id: string;
-  auth_uri: string;
-  token_uri: string;
-  auth_provider_x509_cert_url: string;
-  client_x509_cert_url: string;
-  universe_domain: string;
-};
+import { handleError } from '~/backend/handleError';
+import { HttpError } from '~/backend/HttpError';
+import { createFeedbackSchema } from '~/shared/zodSchemas';
 
 const SCOPES = [
   'https://www.googleapis.com/auth/drive.file',
   'https://www.googleapis.com/auth/spreadsheets',
 ];
 
-const PATH_TO_KEY_FILE = 'steady-scope-436117-p2-f407b01106c9.json';
+const PATH_TO_KEY_FILE =
+  'backend/googleAPI/serviceAccountData/steady-scope-436117-p2-f407b01106c9.json';
 
 const SPREADSHEET_ID = '1mjyeqn3MpaT8mFEN4yHbtOQFK9_KAeCElSaftviKPXE';
 
@@ -34,74 +22,56 @@ export default async function handleFunction(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  /*
-  const auth = new google.auth.GoogleAuth({
-    keyFile: 'steady-scope-436117-p2-f407b01106c9.json',
-    scopes: [
-      'https://www.googleapis.com/auth/drive.file',
-      'https://www.googleapis.com/auth/spreadsheets',
-    ],
-  });
-  //await auth.getClient();
+  try {
+    if (req.method === 'POST') {
+      await handleDataAppending(req, res);
+    } else {
+      throw new HttpError(
+        `${req.method} is not a valid method. Only POST requests are valid!`,
+        405,
+      );
+    }
 
-
-  const sheets = google.sheets({ version: 'v4', auth: auth });
-  const response = await sheets.spreadsheets.values.get({
-    spreadsheetId: '1mjyeqn3MpaT8mFEN4yHbtOQFK9_KAeCElSaftviKPXE',
-    range: `Palautteet!A:A`,
-  });
-  console.log(response);
-  console.log(response.data.values);
-  */
-  //await testFunction();
-  /*
-  const googleApi = new GoogleApiAuthentication({
-    scopes: SCOPES,
-    pathToKeyFile: PATH_TO_KEY_FILE,
-  });
-  */
-  const authentication = await new GoogleApiAuthentication()
-    //.authenticate({ pathToKeyFile: PATH_TO_KEY_FILE, scopes: SCOPES });
-    .authenticate({ pathToKeyFile: PATH_TO_KEY_FILE, scopes: SCOPES });
-  const googleApiSheets = new GoogleApiSheets().initialize({
-    auth: authentication.auth,
-    sheetsVersion: 'v4',
-    spreadsheetId: SPREADSHEET_ID,
-  });
-
-  console.log(
-    googleApiSheets.findSpecificRows({
+    //await googleApiSheets.findSpecificRows({});
+    /*
+    await googleApiSheets.findSpecificRows({
       fromColumn: 'A',
       fromRow: 1,
       toColumn: 'C',
       toRow: 'all',
       spreadsheetId: 'abcdefghijklm',
-    }),
-    'asdasd',
-  );
-  /*
-  console.log('------\n\n\n\n', googleApiSheets, '\n\n\n\n-----');
-  console.log('-----\n\n\n\n', authentication, '\n\n\n\n------');
+    });
+    /*
   */
-  res.status(200).send('Kaikki ok!');
-  return;
+    res.status(200).send('Kaikki ok!');
+    return;
+  } catch (e) {
+    return handleError(res, e);
+  }
 }
 
-async function asd() {
-  const auth = new google.auth.GoogleAuth({
-    keyFile: 'steady-scope-436117-p2-f407b01106c9.json',
-    scopes: [
-      'https://www.googleapis.com/auth/drive.file',
-      'https://www.googleapis.com/auth/spreadsheets',
-    ],
-  });
-  await auth.getClient();
+async function handleDataAppending(req: NextApiRequest, res: NextApiResponse) {
+  const parsedFeedback = createFeedbackSchema
+    .extend({ feedbackDate: z.string() })
+    .parse(req.body);
 
-  const sheets = google.sheets({ version: 'v4', auth: auth });
-  const response = await sheets.spreadsheets.values.get({
-    spreadsheetId: '1mjyeqn3MpaT8mFEN4yHbtOQFK9_KAeCElSaftviKPXE',
-    range: `Palautteet!A:A`,
+  const authentication = await new GoogleApiAuthentication().authenticate({
+    pathToKeyFile: PATH_TO_KEY_FILE,
+    scopes: SCOPES,
   });
-  console.log(response);
-  console.log(response.data.values);
+
+  const googleApiSheets = new GoogleApiSheets().initialize({
+    auth: authentication.auth,
+    sheetsVersion: 'v4',
+    spreadsheetId: SPREADSHEET_ID,
+    sheetName: 'Palautteet',
+  });
+
+  await googleApiSheets.appendData({
+    date: parsedFeedback.feedbackDate,
+    feedback: parsedFeedback.feedbackText,
+    UUID: parsedFeedback.feedbackUUID,
+  });
+
+  res.status(200).send('Request sent succesfully!');
 }
