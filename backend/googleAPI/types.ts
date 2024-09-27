24fffed277c9360c39c18ba9b99d90638abf5f3a
Antti Asmala <antti.asmala@gmail.com>
2024-09-27 10:08:28 +0300

    Add couple more types to GoogleApiSheets_type

--- backend/googleAPI/types.ts
+++ backend/googleAPI/types.ts
@@ -1,5 +1,6 @@
 import { GoogleAuth } from 'google-auth-library';
 import { JSONClient } from 'google-auth-library/build/src/auth/googleauth';
+import { sheets_v4 } from 'googleapis';
 
 export interface GoogleApiAuthentication_type {
   pathToKeyFile?: string;
@@ -10,4 +11,6 @@ export interface GoogleApiAuthentication_type {
 export interface GoogleApiSheets_type extends GoogleApiAuthentication_type {
   sheetsVersion?: string;
   spreadsheetId?: string;
+  sheetName?: string;
+  sheets?: sheets_v4.Sheets;
 }
