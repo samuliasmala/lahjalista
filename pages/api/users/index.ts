75fb84518135450dbefad6dd3c890bd7381012c0
Antti Asmala <antti.asmala@gmail.com>
2024-10-05 14:25:18 +0300

    Add ADMIN check

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -58,10 +58,10 @@ async function handleGET({ res, userData }: HandlerParams<User[]>) {
   return res.status(200).json(users);
 }
 
-// CHECK THIS, voiko tämän poistaa / lisätä, että ei käytössä? Tiliä ei voi luoda, koska ei ole kirjautunut sisään
-// Rekistöröityminen tapahtuu /pages/api/auth/register.ts:n kautta
-// /pages/api/auth/register.ts käyttä createUser-funktiota alempana
-async function handlePOST({ req, res }: HandlerParams<User>) {
+async function handlePOST({ req, res, userData }: HandlerParams<User>) {
+  if (userData.role !== 'ADMIN') {
+    throw new HttpError("You don't have privileges to do that!", 403);
+  }
   const { email, firstName, lastName, password } = createUserSchema.parse(
     req.body,
   );
