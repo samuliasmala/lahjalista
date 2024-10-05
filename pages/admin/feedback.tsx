602af9c5019f1ad581fb824df20cb4f56f9209c2
Antti Asmala <antti.asmala@gmail.com>
2024-10-05 14:20:06 +0300

    Add couple internal components. Add feedback fetcher

--- pages/admin/feedback.tsx
+++ pages/admin/feedback.tsx
@@ -7,21 +7,35 @@ import { handleErrorToast } from '~/utils/handleToasts';
 import { TitleBar } from '~/components/TitleBar';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import axios from 'axios';
+import { Feedback } from '@prisma/client';
 
 export { getServerSideProps };
 
 export default function Home({
   user,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const [feedbacks, setFeedbacks] = useState<Feedback[]>([]);
+  const [currentPage, setCurrentPage] = useState(5);
+  const [totalPages, setTotalPages] = useState(0);
   const [showUserWindow, setShowUserWindow] = useState(false);
 
   const router = useRouter();
 
   useEffect(() => {
-    console.log(user);
     if (!user || user.role !== 'ADMIN') {
       router.push('/');
     }
+    async function fetchFeedbacks() {
+      try {
+        const feedbacks = await (await axios.get('/api/feedback')).data;
+        console.log(feedbacks);
+        setFeedbacks(feedbacks);
+      } catch (e) {
+        handleError(e);
+      }
+    }
+    void fetchFeedbacks();
   }, []);
 
   function handleError(e: unknown) {
@@ -40,8 +54,37 @@ export default function Home({
         userDetails={user}
       />
       <div className="flex flex-row justify-center">
-        <div className="w-full max-w-72"></div>
+        <div className="w-full max-w-72">
+          <FeedbackParagraph />
+        </div>
       </div>
     </main>
   );
+
+  function FeedbackParagraph() {
+    const startNumber = currentPage === 1 ? 0 : (currentPage - 1) * 5;
+    const endNumber = currentPage === 1 ? 5 : startNumber + 5;
+    console.log(startNumber, endNumber);
+    const currentFeedbacks = feedbacks.slice(startNumber, endNumber);
+    if (feedbacks.length > 0) {
+      return (
+        <div>
+          {currentFeedbacks.map((x) => (
+            <>
+              <p>
+                {x.feedbackText}
+                <span> {x.feedbackID}</span>
+              </p>
+              <p>####################################</p>
+            </>
+          ))}
+        </div>
+      );
+    }
+    return null;
+  }
+
+  function PageNavigator() {
+    return <p>PageNavigator</p>;
+  }
 }
