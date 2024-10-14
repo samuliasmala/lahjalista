c6dbb19922966abb46c09f8d0796013922697a32
Antti Asmala <antti.asmala@gmail.com>
2024-10-14 11:43:52 +0300

    Change isUserPhoneUser name. Change how phone user check works. Remove import and comment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,18 +3,13 @@ import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { Bounce, ToastContainer } from 'react-toastify';
 import 'react-toastify/ReactToastify.css';
-import { isPhoneUser } from '~/utils/isPhoneUser';
 import { useEffect, useState } from 'react';
 import { jost } from '~/utils/fonts';
 
-// CHECK THIS, onko tässä tiedostossa mitään järkeä?
-
 export default function App({ Component, pageProps }: AppProps) {
-  const [isUserPhoneUser, setIsUserPhoneUser] = useState(false);
+  const [isPhoneUser, setIsPhoneUser] = useState(false);
   useEffect(() => {
-    if (window) {
-      setIsUserPhoneUser(isPhoneUser(window));
-    }
+    setIsPhoneUser(window.screen.width < 768 || window.screen.height < 768);
   }, []);
 
   return (
@@ -37,7 +32,7 @@ export default function App({ Component, pageProps }: AppProps) {
         pauseOnHover
         theme="light"
         transition={Bounce}
-        limit={isUserPhoneUser ? 1 : 3}
+        limit={isPhoneUser ? 1 : 3}
       />
       <Component {...pageProps} />
     </div>
