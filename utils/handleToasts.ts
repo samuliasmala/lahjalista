4568503e8a7a85cfd3d6cd5a94a5d2cd9c60f7c3
Antti Asmala <antti.asmala@gmail.com>
2024-10-26 17:50:33 +0300

    Change observer to observe classname Toastify, if not found then document.body. Change mutation checks to use getElementById

--- utils/handleToasts.ts
+++ utils/handleToasts.ts
@@ -7,47 +7,23 @@ export function handleErrorToast(errorText: string) {
       toastId: window.crypto.randomUUID(),
     });
 
-    const observer = new MutationObserver((mutationsList) => {
-      for (const mutation of mutationsList) {
-        mutation.addedNodes.forEach((node) => {
-          // check if node is an HTMLElement which unlock a usage of node.children
-          if (node instanceof HTMLElement) {
-            // if there are more than 1 toast, this should be working
-            if (node.id === toastId.toString()) {
-              node.addEventListener('mouseenter', () => {
-                toast.update(toastId, { progress: 1 });
-              });
-              node.addEventListener('mouseout', () => {
-                toast.update(toastId, { progress: 0 });
-              });
-              observer.disconnect();
-              return;
-            }
-
-            // the first toast seems to be always foundable using this for-loop
-            for (let x = 0; x < node.children.length; x++) {
-              const childNode = node.children[x];
-              if (childNode.id === toastId.toString()) {
-                // checking if childNode is an HTMLElement so correct event listener list will be shown
-                // in theory this might not be needed but added for now
-                if (childNode instanceof HTMLElement) {
-                  childNode.addEventListener('mouseenter', () => {
-                    toast.update(toastId, { progress: 1 });
-                  });
-                  childNode.addEventListener('mouseleave', () => {
-                    toast.update(toastId, { progress: 0 });
-                  });
-                  observer.disconnect();
-                  break;
-                }
-              }
-            }
-          }
+    const observer = new MutationObserver(() => {
+      const addedToast = document.getElementById(toastId.toString());
+      if (addedToast) {
+        addedToast.addEventListener('mouseenter', () => {
+          console.log('Mouse enter');
+          toast.update(toastId, { progress: 1 });
         });
+        addedToast.addEventListener('mouseleave', () => {
+          console.log('Mouse out');
+          toast.update(toastId, { progress: 0 });
+        });
+        observer.disconnect();
+        return;
       }
     });
 
-    observer.observe(document.body, {
+    observer.observe(document.querySelector('.Toastify') || document.body, {
       childList: true, // Listen for addition/removal of child nodes
       subtree: true, // Include all child nodes, not just direct children
     });
