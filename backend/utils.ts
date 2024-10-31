ffb074d2b75c044eb88bf86d0d08b26ac999e339
Antti Asmala <antti.asmala@gmail.com>
2024-10-31 16:01:36 +0200

    Create a function that creates a loop for deleting expired session every 24hr

--- backend/utils.ts
+++ backend/utils.ts
@@ -1,4 +1,5 @@
 import { compare as bcryptCompare, hash } from 'bcrypt';
+import { lucia } from './auth';
 
 export async function verifyPassword(
   givenPassword: string,
@@ -12,4 +13,19 @@ export async function hashPassword(password: string): Promise<string> {
   const saltRounds = 10;
   const hashedPassword = await hash(password, saltRounds);
   return hashedPassword;
+}
+
+let loopInterval: NodeJS.Timeout | undefined = undefined;
+
+// 24 hours
+const timeoutTime = 24 * 60 * 60 * 1000;
+
+export async function deleteExpiredSessionsLoop() {
+  if (loopInterval === undefined) {
+    // run delete function immediately, otherwise would need to wait 24 hours for the first run
+    await lucia.deleteExpiredSessions();
+    loopInterval = setInterval(async () => {
+      await lucia.deleteExpiredSessions();
+    }, timeoutTime);
+  }
 }
