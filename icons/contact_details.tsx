e11d0df7f9aa7d50355672cb8b09a1aa13543e9f
Antti Asmala <antti.asmala@gmail.com>
2024-11-11 13:10:51 +0200

    Add a new SVG image. Create a TSX file of it. Add the URL where the SVG was taken to the credits_for_images.txt file

---
+++ icons/contact_details.tsx
@@ -0,0 +1,11 @@
+import type { SVGProps } from 'react';
+const SvgContactDetails = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" {...props}>
+    <path
+      fill="#000"
+      fillRule="evenodd"
+      d="M469.333 85.333v341.334H42.667V85.333zM426.667 128H85.333v256h341.334zM384 298.667v32H277.333v-32zm-194.667-57.6c30.827 0 56 25.6 56 57.6h-128c0-32 25.072-57.6 56-57.6zm194.667-6.4v32H277.333v-32zM181.333 163.96c15.464 0 28 12.536 28 28s-12.536 28-28 28-28-12.536-28-28 12.536-28 28-28M384 170.667v32H277.333v-32z"
+    />
+  </svg>
+);
+export default SvgContactDetails;
