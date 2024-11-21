5e3e69ab7e31e6c9daf68c0198fc90e9eaf1ecf7
Antti Asmala <antti.asmala@gmail.com>
2024-11-21 14:13:01 +0200

    Add some test functionality to /api/persons/[uuid].ts

--- pages/api/persons/[uuid].ts
+++ pages/api/persons/[uuid].ts
@@ -1,17 +1,20 @@
-import { Gift } from '~/shared/types';
+import { Gift, User } from '~/shared/types';
 import { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '~/prisma';
 import { handleError } from '~/backend/handleError';
 import { HttpError } from '~/backend/HttpError';
-import { updateGiftSchema, uuidParseSchema } from '~/shared/zodSchemas';
+import {
+  getPersonSchema,
+  updateGiftSchema,
+  uuidParseSchema,
+} from '~/shared/zodSchemas';
 import { requireLogin } from '~/backend/auth';
-import { User as LuciaUser } from 'lucia';
 
 type HandlerParams<ResponseType = unknown> = {
   req: NextApiRequest;
   res: NextApiResponse<ResponseType>;
-  giftUUID: string;
-  userData: LuciaUser;
+  personUUID: string;
+  userData: User;
 };
 
 const HANDLERS: Record<string, (params: HandlerParams) => Promise<void>> = {
@@ -21,7 +24,7 @@ const HANDLERS: Record<string, (params: HandlerParams) => Promise<void>> = {
   DELETE: handleDELETE,
 };
 
-export default async function handlePrisma(
+export default async function handleRequest(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
@@ -30,12 +33,12 @@ export default async function handlePrisma(
 
     const reqHandler = req.method !== undefined && HANDLERS[req.method];
     if (reqHandler) {
-      const giftUUID = uuidParseSchema.parse(req.query.uuid);
+      const personUUID = uuidParseSchema.parse(req.query.uuid);
 
       await reqHandler({
         req,
         res,
-        giftUUID,
+        personUUID,
         userData,
       });
     } else {
@@ -49,83 +52,25 @@ export default async function handlePrisma(
   }
 }
 
-async function handleGET({ res, giftUUID, userData }: HandlerParams<Gift>) {
-  const gift = await prisma.gift.findUniqueOrThrow({
+async function handleGET({ res, personUUID, userData }: HandlerParams) {
+  const personData = await prisma.person.findUniqueOrThrow({
     where: {
-      uuid: giftUUID,
+      uuid: personUUID,
       userUUID: userData.uuid,
     },
-    select: {
-      createdAt: true,
-      gift: true,
-      receiver: true,
-      updatedAt: true,
-      uuid: true,
-    },
   });
-  return res.status(200).json(gift);
+  const parsedPersonData = getPersonSchema.parse(personData);
+  return res.status(200).json(parsedPersonData);
 }
 
-async function handlePATCH({
-  req,
-  res,
-  giftUUID,
-  userData,
-}: HandlerParams<Gift>) {
-  const giftData = updateGiftSchema.parse(req.body);
-
-  const updatedGift = await prisma.gift.update({
-    where: {
-      uuid: giftUUID,
-      userUUID: userData.uuid,
-    },
-    data: giftData,
-    select: {
-      createdAt: true,
-      gift: true,
-      receiver: true,
-      updatedAt: true,
-      uuid: true,
-    },
-  });
-
-  return res.status(200).json(updatedGift);
+async function handlePATCH({ req, res, personUUID, userData }: HandlerParams) {
+  return res.status(200).send('Test2');
 }
 
-async function handlePUT({
-  req,
-  res,
-  giftUUID,
-  userData,
-}: HandlerParams<Gift>) {
-  const giftData = updateGiftSchema.parse(req.body);
-
-  const updatedGift = await prisma.gift.update({
-    where: {
-      uuid: giftUUID,
-      userUUID: userData.uuid,
-    },
-    data: giftData,
-    select: {
-      createdAt: true,
-      gift: true,
-      receiver: true,
-      updatedAt: true,
-      uuid: true,
-    },
-  });
-
-  return res.status(200).json(updatedGift);
+async function handlePUT({ req, res, personUUID, userData }: HandlerParams) {
+  return res.status(200).send('Test3');
 }
 
-async function handleDELETE({ res, giftUUID, userData }: HandlerParams) {
-  await prisma.gift.delete({
-    where: {
-      uuid: giftUUID,
-      userUUID: userData.uuid,
-    },
-  });
-
-  res.status(200).end();
-  return;
+async function handleDELETE({ res, personUUID, userData }: HandlerParams) {
+  return res.status(200).send('Test4');
 }
