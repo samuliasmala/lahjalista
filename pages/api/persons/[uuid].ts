2e3826788e03bb3d5328964749edbf33734c69e4
Antti Asmala <antti.asmala@gmail.com>
2024-11-25 13:39:45 +0200

    A lot of edits in /persons/[uuid].ts. This was just an idea so that is why it is commited as a big chunk of code

--- pages/api/persons/[uuid].ts
+++ pages/api/persons/[uuid].ts
@@ -4,11 +4,14 @@ import prisma from '~/prisma';
 import { handleError } from '~/backend/handleError';
 import { HttpError } from '~/backend/HttpError';
 import {
+  getAnniversarySchema,
   getPersonSchema,
   updateGiftSchema,
   uuidParseSchema,
+  patchAnniversarySchema,
 } from '~/shared/zodSchemas';
 import { requireLogin } from '~/backend/auth';
+import { z } from 'zod';
 
 type HandlerParams<ResponseType = unknown> = {
   req: NextApiRequest;
@@ -22,6 +25,24 @@ const HANDLERS: Record<string, (params: HandlerParams) => Promise<void>> = {
   PATCH: handlePATCH,
   PUT: handlePUT,
   DELETE: handleDELETE,
+};
+
+type PrismaHandlerParams<ResponseType = unknown> = {
+  res: NextApiResponse<ResponseType>;
+  name: string;
+  date: Date;
+  personUUID: string;
+  userUUID: string;
+  uuid: string;
+};
+
+const PRISMA_ACTION_HANDLERS: Record<
+  string,
+  (params: PrismaHandlerParams) => Promise<void>
+> = {
+  create: handlePrismaCreate,
+  update: handlePrismaUpdate,
+  delete: handlePrismaDelete,
 };
 
 export default async function handleRequest(
@@ -67,11 +88,38 @@ async function handleGET({ res, personUUID, userData }: HandlerParams) {
       userUUID: userData.uuid,
     },
   });
+  console.log(anniversariesOfPerson, personUUID, userData.uuid);
   // ZOD HERE
+  const parsedAnniversaries = z
+    .array(getAnniversarySchema)
+    .parse(anniversariesOfPerson);
+  console.log(parsedAnniversaries);
+
   return res.status(200).json({ parsedPersonData, anniversariesOfPerson });
 }
 
 async function handlePATCH({ req, res, personUUID, userData }: HandlerParams) {
+  console.log(req.body);
+  const parsedAnniversaries = patchAnniversarySchema.parse(
+    req.body.anniversary,
+  );
+  console.log(parsedAnniversaries);
+
+  if (parsedAnniversaries) {
+    const prismaActionHandler =
+      PRISMA_ACTION_HANDLERS[parsedAnniversaries.action];
+
+    const { date, name, uuid } = parsedAnniversaries;
+    return await prismaActionHandler({
+      res,
+      date,
+      name,
+      personUUID,
+      userUUID: userData.uuid,
+      uuid,
+    });
+  }
+
   return res.status(200).send('Test2');
 }
 
@@ -81,4 +129,67 @@ async function handlePUT({ req, res, personUUID, userData }: HandlerParams) {
 
 async function handleDELETE({ res, personUUID, userData }: HandlerParams) {
   return res.status(200).send('Test4');
+}
+
+/*
+handle functions
+*/
+
+async function handlePrismaCreate({
+  res,
+  name,
+  date,
+  personUUID,
+  userUUID,
+}: PrismaHandlerParams) {
+  const createdAnniversary = await prisma.anniversary.create({
+    data: {
+      name,
+      date,
+      personUUID,
+      userUUID,
+    },
+  });
+  // CHECK THIS, ZOD HERE!!!
+  return res.status(200).json(createdAnniversary);
+}
+
+async function handlePrismaUpdate({
+  res,
+  name,
+  date,
+  uuid,
+  personUUID,
+  userUUID,
+}: PrismaHandlerParams) {
+  const updatedAnniversary = await prisma.anniversary.update({
+    where: {
+      uuid: uuid,
+      personUUID: personUUID,
+      userUUID: userUUID,
+    },
+    data: {
+      name: name,
+      date: date,
+    },
+  });
+  console.log(updatedAnniversary);
+  // CHECK THIS, ZOD HERE!!!
+  return res.status(200).json(updatedAnniversary);
+}
+
+async function handlePrismaDelete({
+  res,
+  uuid,
+  personUUID,
+  userUUID,
+}: PrismaHandlerParams) {
+  await prisma.anniversary.delete({
+    where: {
+      uuid,
+      personUUID,
+      userUUID,
+    },
+  });
+  return res.status(200).send('Success!');
 }
