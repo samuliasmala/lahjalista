55f60c73e98977a005cb45fb109900695abb87c3
Antti Asmala <antti.asmala@gmail.com>
2024-12-29 12:06:58 +0200

    Remove unnecessary comment lines. Change function name to follow the file name

--- hooks/useShowErrorToast.ts
+++ hooks/useShowErrorToast.ts
@@ -2,16 +2,7 @@ import { useEffect } from 'react';
 import { handleError } from '~/utils/handleError';
 import { handleErrorToast } from '~/utils/handleToasts';
 
-export function useCatchQueryErrors(error: Error | null) {
-  // Otetaan isError-parametri pois ja jätetään vain error-parametri ja tehdään seuraava tarkistus:
-  /*
-    if(!error) return
-   
-    handleErrorToast(handleError(error))
-    return
-    */
-  // tai jotain vastaavaa
-
+export function useShowErrorToast(error: Error | null) {
   useEffect(() => {
     if (!error) return;
 
