d3140efc3e26f7e000cda230601f92b8906179be
Antti Asmala <antti.asmala@gmail.com>
2025-01-15 14:44:38 +0200

    Remove eslint-disable-next-lines. Change ~/styles/ to ../styles/ to prevent n/no-extraneous-import for now. The rule can be disabled if needed but let's go with this for now

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,7 @@
-// eslint-disable-next-line n/no-extraneous-import
-import '~/styles/globals.css';
+import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { Bounce, ToastContainer } from 'react-toastify';
-// eslint-disable-next-line n/no-extraneous-import
 import 'react-toastify/ReactToastify.css';
 import { useEffect, useState } from 'react';
 import { jost } from '~/utils/fonts';
