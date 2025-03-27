f6ce59325718ab1e010a4d48efb3bc051ea0f0af
Antti Asmala <antti.asmala@gmail.com>
2025-03-27 13:09:31 +0200

    Add new file useIsAdmin.ts inside hooks

---
+++ hooks/useIsAdmin.ts
@@ -0,0 +1,21 @@
+import { NextRouter } from 'next/router';
+import { useEffect } from 'react';
+import { User } from '~/shared/types';
+
+/**
+ * This useEffect checks if the User has the ADMIN role
+ *
+ * if the User does not have the ADMIN role, they will be redirected back to main page
+ *
+ * else the User can access the page
+ * @param user User
+ * @param router NextRouter
+ */
+export function useIsAdmin(user: User, router: NextRouter) {
+  useEffect(() => {
+    if (user.role !== 'ADMIN') {
+      router.push('/').catch((e) => console.error(e));
+      return;
+    }
+  }, [user]);
+}
