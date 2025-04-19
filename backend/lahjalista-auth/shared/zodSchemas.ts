5c538c1331d90bc57d91ecdbfb94e07dce0c6dd3
Antti Asmala <antti.asmala@gmail.com>
2025-04-19 19:39:05 +0300

    Add make variables exportable again

--- backend/lahjalista-auth/shared/zodSchemas.ts
+++ backend/lahjalista-auth/shared/zodSchemas.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 import { getUserSchema } from '~/shared/zodSchemas';
 
-const sessionSchema = z.object({
+export const sessionSchema = z.object({
   uuid: z.string().uuid(),
   userUUID: z.string().uuid(),
   expiresAt: z.date(),
@@ -10,21 +10,21 @@ const sessionSchema = z.object({
   updatedAt: z.date(),
 });
 
-const databaseSessionSchema = sessionSchema;
+export const databaseSessionSchema = sessionSchema;
 
-const createSessionSchema = sessionSchema;
+export const createSessionSchema = sessionSchema;
 
-const frontendSessionSchema = databaseSessionSchema
+export const frontendSessionSchema = databaseSessionSchema
   .pick({ uuid: true, expiresAt: true, isLoggedIn: true })
   .extend({ fresh: z.boolean() });
 
-const validSessionResultSchema = z.object({
+export const validSessionResultSchema = z.object({
   status: z.literal('valid'),
   databaseSession: databaseSessionSchema,
   databaseUser: getUserSchema,
 });
 
-const invalidSessionResultSchema = z.object({
+export const invalidSessionResultSchema = z.object({
   status: z.literal('invalid'),
   databaseSession: databaseSessionSchema,
   databaseUser: getUserSchema,
