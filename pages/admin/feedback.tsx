6945172ed50c5836659078346247c6211ad3a295
Antti Asmala <antti.asmala@gmail.com>
2025-05-25 18:37:43 +0300

    Clean some code from feedback.tsx

--- pages/admin/feedback.tsx
+++ pages/admin/feedback.tsx
@@ -1,19 +1,10 @@
-import {
-  Dispatch,
-  ReactNode,
-  SetStateAction,
-  useEffect,
-  useState,
-} from 'react';
-import { Button } from '~/components/Button';
+import { useState } from 'react';
 import { InferGetServerSidePropsType } from 'next';
 import { getServerSidePropsAdminOnly as getServerSideProps } from '~/utils/getServerSideProps';
 import { TitleBar } from '~/components/TitleBar';
 import axios from 'axios';
-import { useKeyPress } from '~/hooks/useKeyPress';
 import { useQuery } from '@tanstack/react-query';
 import { QueryKeys, Feedback } from '~/shared/types';
-import { useShowErrorToast } from '~/hooks/useShowErrorToast';
 
 // ALLOWS ADMINS ONLY, look at the import for more details!
 export { getServerSideProps };
@@ -21,21 +12,7 @@ export { getServerSideProps };
 export default function Home({
   user,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  const [currentPage, setCurrentPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(0);
   const [showUserWindow, setShowUserWindow] = useState(false);
-
-  useKeyPress('ArrowRight', () => {
-    setCurrentPage((currentPageNumber) =>
-      calculatePageSwitch(currentPageNumber, totalPages, 'plus'),
-    );
-  });
-
-  useKeyPress('ArrowLeft', () => {
-    setCurrentPage((currentPageNumber) =>
-      calculatePageSwitch(currentPageNumber, totalPages, 'minus'),
-    );
-  });
 
   const { data: feedbacks, error } = useQuery({
     queryKey: QueryKeys.ADMIN_FETCH_FEEDBACKS,
@@ -46,17 +23,6 @@ export default function Home({
     },
   });
 
-  useShowErrorToast(error);
-
-  // sets totalpage amount
-  useEffect(() => {
-    if (feedbacks) {
-      // Rounds up the value, eg. 21 feedbacks / 5 = 4.2 -> 5
-      // the divider (5) will be changed to generic in the future which allows user to determine how many feedbacks should be shown per page
-      setTotalPages(Math.ceil(feedbacks.length / 5));
-    }
-  }, [feedbacks]);
-
   return (
     <main className="h-screen w-full max-w-full">
       <div className="flex min-h-screen flex-col">
@@ -65,152 +31,8 @@ export default function Home({
           showUserWindow={showUserWindow}
           userDetails={user}
         />
-        <div className="flex flex-col">
-          <div className="w-full max-w-72 self-center">
-            <FeedbackParagraph
-              currentPage={currentPage}
-              feedbacks={feedbacks}
-            />
-          </div>
-        </div>
-        <Footer>
-          <PageNavigator
-            currentPage={currentPage}
-            feedbacks={feedbacks}
-            setCurrentPage={setCurrentPage}
-            totalPages={totalPages}
-          />
-        </Footer>
+        <div className="flex flex-col"></div>
       </div>
     </main>
-  );
-}
-
-// CHECK THIS, olisiko hyvä nakkaa esim. /utils/utilFunctions.ts-tiedostoon
-function calculatePageSwitch(
-  currentPage: number,
-  totalPages: number,
-  plusOrMinus: 'plus' | 'minus',
-): number {
-  if (plusOrMinus === 'plus')
-    return currentPage + 1 <= totalPages ? currentPage + 1 : 1;
-
-  return currentPage - 1 >= 1 ? currentPage - 1 : totalPages;
-}
-
-// this handles the individual Feedback. Code loops through all of feedbacks
-// and creates same looking elements of them
-function FeedbackParagraph({
-  currentPage,
-  feedbacks,
-}: {
-  currentPage: number;
-  feedbacks?: Feedback[];
-}) {
-  // this will be changed to be a generic in the future
-  const howManyFeedbacksPerPage = 5;
-
-  // if currentPage is 1, we want to startNumber be 0 so we can get
-  const startNumber =
-    currentPage === 1 ? 0 : (currentPage - 1) * howManyFeedbacksPerPage;
-
-  const endNumber =
-    currentPage === 1
-      ? howManyFeedbacksPerPage
-      : startNumber + howManyFeedbacksPerPage;
-
-  if (!feedbacks || feedbacks.length <= 0) {
-    return null;
-  }
-
-  const currentFeedbacks = feedbacks.slice(startNumber, endNumber);
-
-  if (feedbacks.length > 0) {
-    return (
-      <div className="w-full">
-        {currentFeedbacks.map((x, index) => (
-          <div
-            className="mb-4 whitespace-pre-line border-4 [overflow-wrap:anywhere]"
-            key={index}
-          >
-            <p>{x.feedbackText}</p>
-          </div>
-        ))}
-      </div>
-    );
-  }
-  return null;
-}
-
-// this handles the number in bottom bar ("Footer") and the
-// next and previous buttons
-function PageNavigator({
-  setCurrentPage,
-  currentPage,
-  feedbacks,
-  totalPages,
-}: {
-  setCurrentPage: Dispatch<SetStateAction<number>>;
-  currentPage: number;
-  feedbacks?: Feedback[];
-  totalPages: number;
-}) {
-  return (
-    <div className="flex h-full w-full items-center justify-center">
-      <Button
-        className="text-md m-0 mr-4 h-10 w-24 p-0"
-        onClick={() =>
-          setCurrentPage((currentPage) => {
-            return calculatePageSwitch(currentPage, totalPages, 'minus');
-          })
-        }
-      >
-        {'<'} Edellinen
-      </Button>
-      <div>
-        {totalPages <= 1 ? (
-          currentPage
-        ) : (
-          <span>
-            <select
-              onChange={(e) => {
-                setCurrentPage(Number(e.target.value) || 1);
-              }}
-              value={currentPage}
-            >
-              {feedbacks?.map((_, index) => {
-                if (index <= totalPages && index > 0) {
-                  return (
-                    <option value={index} key={index}>
-                      {index}
-                    </option>
-                  );
-                }
-                return null;
-              })}
-            </select>
-          </span>
-        )}{' '}
-        / {totalPages}{' '}
-      </div>
-      <Button
-        className="text-md m-0 ml-4 h-10 w-24 p-0"
-        onClick={() =>
-          setCurrentPage((currentPage) => {
-            return calculatePageSwitch(currentPage, totalPages, 'plus');
-          })
-        }
-      >
-        Seuraava {'>'}
-      </Button>
-    </div>
-  );
-}
-
-function Footer({ children }: { children?: ReactNode }) {
-  return (
-    <div className="sticky bottom-0 mt-auto h-24 w-full self-center bg-primaryLight sm:w-96">
-      {children}
-    </div>
   );
 }
